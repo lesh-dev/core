1fac1c4f2951aa90d7b7937fa3618d881bf0617d
Alexander Trousevich <trousev@yandex.ru>
2012-03-14 20:24:24 +0400

    updates. gitignore changes.

--- site/Engine/cms/menu/.func.php
+++ site/Engine/cms/menu/.func.php
@@ -62,14 +62,15 @@ usort($html_array,"sort_by_mtime");
     {
       $margin = $menuLevel*10;
       $html = "<br><a style=\"margin-left: {$margin}pt;\" href=\"<HREF>\">[$flags<TEXT>]</a>";
+      $html = str_replace("<HREF>","/$web_prefix?page=$pageiid&$addhrefparams",$html);
     }
-    if(file_exists("$initPath/currtag"))
+    else if(file_exists("$initPath/currtag"))
     {
 	$tag = file_get_contents("$initPath/currtag");
 	$html = str_replace("<HREF>","/$web_prefix$tag/$addhrefparams",$html);
     }
     else
-	$html = str_replace("<HREF>","?page=$pageiid&$addhrefparams",$html);
+	$html = str_replace("<HREF>","/$web_prefix?page=$pageiid&$addhrefparams",$html);
     $html = str_replace("<TEXT>",$text,$html);
     
     if(strstr($pageid,str_replace("{$SETTINGS["datadir"]}cms/pages/","",$initPath)))
