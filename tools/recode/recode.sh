37a4dfe3b513352eb9c91a19cb9eb79b0ec9aa78
Alexander Trousevich <trousev@yandex.ru>
2012-03-14 20:22:01 +0400

    yet more updates

---
+++ tools/recode/recode.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+	for i in $(find -type f  | grep -v recode); do
+	  #echo $i
+	  enca $i | grep 1251  1> /dev/null
+	  if [ $? == 0 ]; then
+	    cp $i recode
+	    cat recode | enconv > $i
+	  fi
+	done
