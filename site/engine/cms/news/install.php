eb56527b949e1900d376d683cd27f3716d1a5b28
root <root@wpm.org.ru>
2012-03-15 02:17:08 +0300

    testing branch: automatic testing stand back-merge

--- site/engine/cms/news/install.php
+++ site/engine/cms/news/install.php
@@ -4,6 +4,15 @@
   $INFO["installed"] = "ok";
   $INFO["maxNews"] = 10;
   saveList($INFO,"{$SETTINGS["datadir"]}cms/pages/$pageid/info");
+  $f = fopen("{$SETTINGS["datadir"]}cms/pages/$pageid/template");
+  fputs($f,
+'
+  <div class="newstitle"><a href="<STAMP>"><DATE>. Header</a></div>
+    <p class="text">text</p>
+  <div class="signature"><LOGIN></div>
+'
+  );
+  fclose($f);
   //saveList($LIST,"{$SETTINGS["datadir"]}cms/pages/$pageid/info");
   //echo '<script>alert("раздел новостей '.$pageid.' создан и инициализирован.");</script>';
   
--- site/settings.php
+++ site/settings.php
@@ -2,5 +2,6 @@
     $content_dir = "fizlesh.ru-content/";
     $design_dir = "fizlesh.ru-design/";
     $engine_dir = "engine/";
+    $engine_pub = "engine_public/";
     $web_prefix="testing/site/";
 ?>
