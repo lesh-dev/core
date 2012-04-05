9b5997be7da0d445e594514c013fea8b456ed281
Alexander Trousevich <trousev@yandex.ru>
2012-04-06 00:10:52 +0400

    fixed: #130

---
+++ site/engine/.sys/groups.php
@@ -0,0 +1,18 @@
+<?
+    function all_groups()
+    {
+	global $content_dir;
+        $ans = array(
+	    "#all" => "Кто угодно"
+            ,"#registered" => "Зарегистрированный"
+            ,"#admin" => "Администратор"
+            ,"#editor" => "Редактор"
+            ,"#ank" => "Менеджер анкет"
+        );
+        $A = @getList("$content_dir/groups");
+        if($A) foreach($A as $k=>$v)
+    	    $ans[$k] = $v;
+        return $ans;
+    }
+    
+?>
