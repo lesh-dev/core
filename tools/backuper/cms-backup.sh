754e74811041844463c3957d471037c28fe0f1b1
vdm <vdm-photo@ya.ru>
2012-04-13 23:38:33 +0400

    merge tools to master

--- tools/backuper/cms-backup.sh
+++ tools/backuper/cms-backup.sh
@@ -2,8 +2,12 @@
 LOG="fizlesh.ru-backup.log"
 echo "== `date`:Backup process started  ==" >> $LOG
 rm -rf ./fizlesh.ru
-wget -q -r -np -R '*.jpeg,*.jpg,*.html' -X /data/cms/pages/z01News,/data/cms/pages/z065Photoalbum http://fizlesh.ru/data/cms/pages/ 
+wget --user="dmvn@speleoastronomy.org" --password="cCkYAxtHdU5g" -q -r -np -R '*.jpeg,*.jpg,*.html' -X /data/cms/pages/z01News,/data/cms/pages/z065Photoalbum ftp://fizlesh.ru/data/cms/pages/z025AboutUs 
 echo "   `date`:Content download complete" >> $LOG
+wget --user="dmvn@speleoastronomy.org" --password="cCkYAxtHdU5g" -q -r -np -R '*.jpeg,*.jpg,*.html' ftp://fizlesh.ru/ank
+echo "   `date`:Anketas download complete" >> $LOG
+wget --user="dmvn@speleoastronomy.org" --password="cCkYAxtHdU5g" -q -r -np -R '*.jpeg,*.jpg,*.html' ftp://fizlesh.ru/data/auth/usr
+echo "   `date`:Usr dir download complete" >> $LOG
 hg add
 hg commit -m "daily backup" -u autobackuper
 echo "   `date`:Commited to repo" >> $LOG
