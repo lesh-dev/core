72167caa59fe913aebac8b1ad3c5d338e08c67c7
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2012-05-08 15:43:37 +0400

    Added mailerEnabled = false to settings, not to send lots of spam while debugging.
    Default value of this setting is not defined, but assumed it's true.

--- site/engine/.sys/mailer.php
+++ site/engine/.sys/mailer.php
@@ -1,23 +1,26 @@
 <?php
-  function send_notify_mail($target_list,$subject,$mail_text)
-  {
-    global $SETTINGS,$login;
-    $message = $mail_text;
-    $message = wordwrap($message, 160);
-    $list = getList("{$SETTINGS["datadir"]}cms/mailer.conf");
-    $ml = $list[$target_list];
-    if(!  $ok= @mail($ml, $subject,
-        "$message"."\r\n".
-        "-- \n".
-        "Исполнитель     : $login\n".
-        "Имя хоста       : ".php_uname('n')." \n".
-        "Обратная ссылка : {$_SERVER['HTTP_REFERER']}\n".
-        ""
-        ,
-        "From: xcms [".php_uname('n')."] mailer  <noreply@fizlesh.ru>\r\n".
-        'Content-Type: text/plain; charset=utf-8'."\r\n".
-        "Content-Transfer-Encoding: 8bit\r\n"
-    ))
-      echo "<li>SENDMAIL FAILED.";
-  }
+    function send_notify_mail($targetList, $subject, $mailText)
+    {
+        global $SETTINGS, $login;
+        if (array_key_exists("mailerEnabled", $SETTINGS) && $SETTINGS["mailerEnabled"] === false) return;
+        $message = $mailText;
+        $message = wordwrap($message, 160);
+        $list = getList("{$SETTINGS["datadir"]}cms/mailer.conf");
+        $ml = $list[$targetList];
+
+        $ok = @mail($ml, $subject,
+            "$message"."\r\n".
+            "-- \n".
+            "Исполнитель     : $login\n".
+            "Имя хоста       : ".php_uname('n')." \n".
+            "Обратная ссылка : {$_SERVER['HTTP_REFERER']}\n".
+            ""
+            ,
+            "From: xcms [".php_uname('n')."] mailer  <noreply@fizlesh.ru>\r\n".
+            'Content-Type: text/plain; charset=utf-8'."\r\n".
+            "Content-Transfer-Encoding: 8bit\r\n"
+        );
+
+        if (!$ok) echo "<li>SENDMAIL FAILED.";
+    }
 ?>
