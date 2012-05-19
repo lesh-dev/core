752bbcb728f0f42e750d83396737d7740ea692a0
vdm <vdm-photo@ya.ru>
2012-05-19 16:31:53 +0400

    link validator (ticket #136 done)

---
+++ tools/link-validator/link-validate.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# (c) fizlesh.ru dev team
+# maintainer: vdm (vdm-photo@ya.ru)
+
+if ! [ "$1" ]; then
+	echo "This script validates specified site and checks link integrity."
+	echo "Syntax: $0 <url-to-validate>"
+	exit 1
+fi
+
+URL="$1"
+LOG="validate.log"
+BROKEN="broken.log"
+wget --exclude-directories="/forum" --no-verbose --spider -r $URL -o "$LOG"
+
+if grep -B2 "broken link!" "$LOG" > "$BROKEN"; then
+	echo "ERROR! Broken links detected! See files '$BROKEN' and '$LOG' for details."
+	exit 1
+else
+	echo "No broken links found"
+	rm -f "$BROKEN"
+	exit 0
+fi
