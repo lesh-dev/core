79c3fbfcb519bc9796c6a3f40d9f9149e469473c
vdm <vdm-photo@ya.ru>
2012-05-28 01:52:06 +0400

    email plugin fixed.

--- site/engine/plugins/dollar/email.php
+++ site/engine/plugins/dollar/email.php
@@ -1,3 +1,3 @@
 <?php
-	echo "<img src=\"$content_prefix/pic/email/$_0.png\" class=\"email\" alt=\"$_0's box\"></img>";
+	echo "<img src=\"/$web_prefix/$content_dir/pic/email/$_0.png\" class=\"email\" alt=\"$_0's box\"></img>";
 ?>
