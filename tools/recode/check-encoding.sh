e480c5ef97619fb03ac8e0b5620f017b6faec199
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2012-05-31 12:42:26 +0400

    Fixed last one file with invalid encoding. Improved encoding-detection script.

--- tools/recode/check-encoding.sh
+++ tools/recode/check-encoding.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-find . -type f | xargs enca -L russian |  grep 1251
+find . -type f -size +0 | egrep -v '(\.pdf|\.jpg|\.png|\.jpeg|\.gz|\.gif|\.ico|\.avi|\.bz2)' | xargs \
+    enca -L russian | egrep '(1251|Unrecognized)'
