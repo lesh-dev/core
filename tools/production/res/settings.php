5bd01db98ae15a851987d400029d5d3df4154721
root <root@fizlesh.ru>
2012-06-14 23:44:39 +0400

    server production tools added

---
+++ tools/production/res/settings.php
@@ -0,0 +1,8 @@
+<?php
+    $content_dir = "content/";
+    $design_dir = "fizlesh.ru-design/";
+    $engine_dir = "engine/";
+    $engine_pub = "engine_public/";
+    $web_prefix = "";
+    $mailer_enabled = true;
+?>
