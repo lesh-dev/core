b0b4dd69873e4c5581c75318104bfeda7135ee59
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2012-06-27 02:40:53 +0400

    Fixed #198 (at least it's not so ugly now).
    However, we don't know how to perform a conditional rendering
    in both cases when we already output something and, the second
    case, when page is empty. This leads to invalid HTML, and this
    problem should be resolved separately. As a solution we can
    have a flag that controls output state (except headers), so each
    auth form can control what to do: either render the complete body
    or behave like a widget.

--- site/install.mvel.sh
+++ site/install.mvel.sh
@@ -11,6 +11,7 @@ fi
 sudo mkdir -p "$dest"
 sudo rm -rf /var/www/html/site/*
 sudo cp -a * "$dest/"
+sudo cp -a ../junk/fizlesh.ru-content "$dest/"
 sudo rm -rf "$dest/.prec/"*
 sudo rm -rf "$dest/admin_doc/.prec/"*
 sudo mkdir -p "$dest/.prec/"
