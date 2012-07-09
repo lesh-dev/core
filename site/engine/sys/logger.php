384be5bff265d67d44fbfc9f8f7ae04830359c1c
Mikhail Veltishchev <mvel@lambda.yandex.ru>
2012-07-10 01:34:50 +0400

    An attempt to fix non-expiring cookies. Logger for arrays.

--- site/engine/sys/logger.php
+++ site/engine/sys/logger.php
@@ -13,4 +13,15 @@
         fwrite($f, "[".date('Y.m.d H:i:s')."]: $message\n");
         fclose($f);
     }
+
+    function xcms_log_array($log_level, $name, $array)
+    {
+        global $engine_dir;
+        $f = fopen("$engine_dir/../engine.log", "a+");
+        fwrite($f, "[".date('Y.m.d H:i:s')."]: ARRAY $name DUMP\n");
+        foreach ($array as $key => $value)
+            fwrite($f, "    '$key' => '$value'\n");
+        fclose($f);
+    }
+
 ?>
