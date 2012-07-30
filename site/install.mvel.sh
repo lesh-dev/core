8394fd511785218762a6fc8c83ff1d0583b2ccc2
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2012-07-31 02:48:31 +0400

    Converter implemented as 'all' anketas lister.

--- site/install.mvel.sh
+++ site/install.mvel.sh
@@ -18,11 +18,12 @@ sudo cp fizlesh.sqlite3 "$dest/fizlesh.ru-content/ank/"
 sudo rm -rf "$dest/.prec/"*
 sudo rm -rf "$dest/admin_doc/.prec/"*
 sudo mkdir -p "$dest/.prec/"
+sudo mkdir -p "$dest/aaa/"
 sudo mkdir -p "$dest/admin_doc/.prec/"
 sudo touch "$dest/"{.htaccess,settings.php,engine.log}
 if [ "$1" = "-f" ] ; then
     sudo touch "$dest/install.php"
     sudo chown -R apache:apache "$dest/install.php"
 fi
-sudo chown -R apache:apache "$dest/"{.prec,fizlesh.ru-content,.htaccess,settings.php,engine.log}
+sudo chown -R apache:apache "$dest/"{aaa,.prec,fizlesh.ru-content,.htaccess,settings.php,engine.log}
 sudo chown -R apache:apache "$dest/admin_doc/"{.prec,content,.htaccess,settings.php}
