bf96610de212020c6c04c4809e1f9b6100525372
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2012-08-03 14:41:25 +0400

    Added lesh-updater cron script.

---
+++ tools/backuper/mvel/lesh-update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -xe
+cd /var/www/html/lesh
+if [ -d lesh.git ] ; then
+    mv lesh.git .git
+fi
+if ! [ -d .git ] ; then
+    echo "Something is a miss here"
+    exit 1
+fi
+log=../lesh-update.log
+echo '* * * Update state for lesh repo' > $log
+date >> $log
+echo 'Last 5 commits were:' >> $log
+( git pull && git checkout -- * && git log -n 5 ) >> $log || true
+mv .git lesh.git || true
