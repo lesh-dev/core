87bcff9ffa1d26997062c11d139384e459a2a171
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2012-09-23 10:28:10 +0400

    My install script works again.
    Fixes applied after brutal admin_doc removal.
    Installing canonical dump from L12.

--- site/install.mvel.sh
+++ site/install.mvel.sh
@@ -13,8 +13,9 @@ rm -f $db || true
 #sqlite3 $db < dbinit.sql
 #sqlite3 $db < unused-dbinit.sql
 #sqlite3 $db < ../junk/fizlesh.ru-content/ank/anketas-2012.07.23.sql
+cp ../junk/fizlesh.ru-content/ank/lesh-2012.08.30.sqlite3 $db
 #cp /var/www/html/site/fizlesh.ru-content/ank/fizlesh.sqlite3 complete.sql
-cp complete.sql $db
+#cp complete.sql $db
 
 sudo mkdir -p "$dest"
 sudo rm -rf /var/www/html/site/*
@@ -22,13 +23,10 @@ sudo cp -a * "$dest/"
 sudo cp -a ../junk/fizlesh.ru-content "$dest/"
 sudo cp $db "$dest/fizlesh.ru-content/ank/"
 sudo rm -rf "$dest/.prec/"*
-sudo rm -rf "$dest/admin_doc/.prec/"*
 sudo mkdir -p "$dest/.prec/"
-sudo mkdir -p "$dest/admin_doc/.prec/"
 sudo touch "$dest/"{.htaccess,settings.php,engine.log}
 if [ "$1" = "-f" ] ; then
     sudo touch "$dest/install.php"
     sudo chown -R apache:apache "$dest/install.php"
 fi
 sudo chown -R apache:apache "$dest/"{.prec,fizlesh.ru-content,.htaccess,settings.php,engine.log}
-sudo chown -R apache:apache "$dest/admin_doc/"{.prec,content,.htaccess,settings.php}
