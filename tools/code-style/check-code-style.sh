4293a7fb77fe6a0dfcc797e002b04949d026c5ef
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2012-09-23 11:02:20 +0400

    Added simple ignore list to code style checker.

--- tools/code-style/check-code-style.sh
+++ tools/code-style/check-code-style.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
+# We should not ignore any errors, it's better to fail
+# Please do not remove the following line
+set -e
 
-# Checks that no tabs uses in code
-#set -xe
+# Runs python-based code style checker
 
 path="."
 my_base="`dirname $0`"
 fail=
 
+# ignore list (contrib files whose code style
+# we don't want to verify
+ignore_list="class.phpmailer.php"
+
 check_style()
 {
     for i in `find $path -type f -name "$1"`; do
+        # skip ignored files
+        ib="`basename $i`"
+        for b in $ignore_list ; do
+            if [ "$ib" == "$b" ] ; then
+                ignore="yes"
+                break
+            fi
+        done
+        if [ "$ignore" == "yes" ] ; then
+            continue
+        fi
+        # check files
         if ! $my_base/check.py $i > /tmp/check-result ; then
             echo "*** Checking '$i' failed:"
             cat /tmp/check-result
