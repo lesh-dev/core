859d1c0a06e28807de2994a4243ed5cf7abdfc99
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2012-09-27 01:11:34 +0400

    sitemeta.php install hook was code-styled.

--- site/engine/install_hooks/sitemeta.php
+++ site/engine/install_hooks/sitemeta.php
@@ -1,77 +1,86 @@
 <?php
     /**
-      Это пример "инсталл-хука". Вы можете сделать 
-      такой же, если Вы хотите контролировать 
-      процедуру установки.
-    **/
+      * Хук настройки метаинформации о сайте (имя сайта и т.п.)
+      **/
     class MetaInfoHook
     {
-        /** 
-          Эта функция определяет название модуля 
-          и не делает ничего больше **/
-	function module_name()
-	{
-	    return "Общая информация о веб-сайте";
-	}
-	/** 
-	  Эта функция должна вернуть список переменных, 
-	  необходимых модулю для функционирования
-	**/
-	function request_variables()
-	{
-	    return array(
-	      "site_name"=>array("name"=>"Название веб-сайта","type"=>"string","default"=>$_SERVER["HTTP_HOST"])
-	      ,"site_url"=>array("name"=>"Адрес веб-сайта","type"=>"string","default"=>"http://{$_SERVER['HTTP_HOST']}".str_replace("install.php","",$_SERVER["REQUEST_URI"]))
-	      ,"webmaster_mail"=>array("name"=>"Адрес службы техподдержки","type"=>"string","default"=>"support@".$_SERVER["HTTP_HOST"])
-	    );
-	    //return array();
-	}
-	/**
-	  Эта функция будет вызвана для проверки, 
-	  удовлетворяет ли система требованиям модуля. 
-	  Должна вернуть true если да, или строку с 
-	  описанием ошибки, если нет.
-	**/
-	function initial_check()
-	{
-	    return true;
-	}
-	/**
-          Эта функция будет вызвана для проверки после 
-          того, как пользователь введет дополнительные 
-          параметры. Должна вернуть true если все в порядке 
-          и можно начинать установку, или строку с описанием 
-          ошибки, если нет.
-	**/
-	function final_check($config)
-	{
-	    return true;
-	}
-	/** 
-          Эта функция должна полностью подчистить следы использования модуля.
-          Например, если инсталлятор дописывает что-то в файл, то этот метод должен файл удалить.
-	**/
-	function uninstall()
-	{
-            return true;
-	}
-	/**
-	  Самый Главный Метод. Собственно, именно в нем можно что-нибудь сделать с устанавливаемой системой.
-	**/
-	function install($config)
-	{
-	    $f = fopen("settings.php","a");
-	    fputs($f,"\n<?php /** This block was inserted by installer -- sitemeta.php. Edit it, but it can be regenerated. **/");
-	    fputs($f,"\n   \$meta_site_name = '".$config["site_name"]."';");
-	    fputs($f,"\n   \$meta_site_url = '".$config["site_url"]."';");
-	    fputs($f,"\n   \$meta_site_mail = '".$config["webmaster_mail"]."';");
-	    fputs($f,"\n/** sitemeta.php hook : end **/ ?>\n");
-	    return true;
-	}
-    }
-    
     /**
-      Объект необходимо инстанциировать в переменную с названием $hook.
-    **/
+      * Определяет название модуля
+      **/
+    function module_name()
+    {
+        return "Общая информация о веб-сайте";
+    }
+    /**
+      * Возвращает список переменных,
+      * необходимых модулю для функционирования
+      **/
+    function request_variables()
+    {
+        return array(
+            "site_name"=>array(
+                "name"=>"Название веб-сайта",
+                "type"=>"string",
+                "default"=>$_SERVER["HTTP_HOST"]
+                ),
+            "site_url"=>array(
+                "name"=>"Адрес веб-сайта",
+                "type"=>"string",
+                "default"=>"http://{$_SERVER['HTTP_HOST']}".str_replace("install.php","",$_SERVER["REQUEST_URI"])
+                ),
+            "webmaster_mail"=>array(
+                "name"=>"Адрес службы техподдержки",
+                "type"=>"string",
+                "default"=>"support@".$_SERVER["HTTP_HOST"])
+        );
+    }
+    /**
+      * Требования этого модуля тривиальны
+      * @return true в любом случае
+      */
+    function initial_check()
+    {
+        return true;
+    }
+    /**
+      * Проверяем введённые параметры
+      * Пока что оформлена в виде заглушки.
+      * TODO: Сделать минимальную проверку вводимой информации
+      **/
+    function final_check($config)
+    {
+        return true;
+    }
+    /**
+      * Удаление модуля: подчищать тут нечего, файлов мы не создаём
+      * @return true в любом случае
+      **/
+    function uninstall()
+    {
+        return true;
+    }
+    /**
+      * Дописываем в settings.php блок метаинформации
+      * @return true, если у нас это получилось
+      **/
+    function install($config)
+    {
+        $f = fopen("settings.php", "a+");
+        if (!$f)
+            return "Cannot open settings.php for append. ";
+
+        fputs($f, "\n<?php /* This block was inserted by installer -- sitemeta.php.");
+        fputs($f, "\nYou may edit it, but it can be regenerated. */");
+        fputs($f, "\n   \$meta_site_name = '".$config["site_name"]."';");
+        fputs($f, "\n   \$meta_site_url = '".$config["site_url"]."';");
+        fputs($f, "\n   \$meta_site_mail = '".$config["webmaster_mail"]."';");
+        fputs($f, "\n/* --- */ ?>\n");
+        return true;
+    }
+    } // class
+
+    /**
+      * Объект необходимо инстанциировать в переменную с названием $hook.
+      **/
     $hook = new MetaInfoHook();
 ?>
