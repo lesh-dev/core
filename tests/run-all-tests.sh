b176b7f8f185f1d8f7fbe2a70477b5d0d1176e7d
Dmitry Veltishev <vdm-photo@ya.ru>
2012-10-11 11:27:51 +0400

    helper class for selenuim test introduced.

--- tests/run-all-tests.sh
+++ tests/run-all-tests.sh
@@ -2,11 +2,22 @@
 
 set -e 
 
+TARGET_SITE="http://test.fizlesh.ru"
+
+LOG="test.log"
+
 run_test()
 {
-	echo "Running test $1"
-	python $1
+	echo "========== Running test $1 ==========" >> "$LOG"
+	if ! python $1 "$TARGET_SITE" >> "$LOG"; then
+		echo "Test $1 FAILED";
+	else
+		echo "Test $1 PASSED"
+	fi
+	
 }
+
+echo "Test suite started on `date +%Y-%m-%d-%H:%M:%S`" > "$LOG"
 
 for i in ./*.py; do
 	run_test xcms-open-non-existing.py
--- tests/xcms-open-non-existing.py
+++ tests/xcms-open-non-existing.py
@@ -6,36 +6,68 @@ import os, sys, traceback
 
 debugMode = True #"TRUE" in os.getenv("XCMS_TEST_DEBUG");
 
-driver = webdriver.Firefox()
+class SeleniumTest:
+	def __init__(self, baseUrl = None):
+#		print "Init SeleniumTest"
+		if baseUrl is None:
+			if len(sys.argv) < 2:
+				raise RuntimeError("Base URL for test is not set. ")
+			else:
+				baseUrl = sys.argv[1];
+				
+		self.m_driver = webdriver.Firefox()
+		self.m_baseUrl = baseUrl;
+		
+	def __del__(self):
+#		print "Destructing SeleniumTest"
+		if hasattr(self, 'm_driver'):
+			self.m_driver.close()
+		
+	def page(self, url):
+		
+		# detect if URL is full
+		fullUrl = self.m_baseUrl + url
+			
+		print "Navigating to " + fullUrl
+		self.m_driver.get(fullUrl);
+	
+	def site(self, fullUrl):
+		print "Going to site " + fullUrl
+		self.m_driver.get(fullUrl)
+		
+	def drv(self):
+		return self.m_driver;
+		
+	def checkTextPresent(self, xpath, text):
+		ele = self.m_driver.find_element_by_xpath(xpath)
+		return text in ele.text;
+		
+	def assertTextPresent(self, xpath, text):
+		if not self.checkTextPresent(xpath, text):
+			raise RuntimeError("Text '" + text + "' not appers on page in element " + xpath)
+			
+	def getUrlByLinkText(self, urlText):
+		url = self.m_driver.find_element_by_link_text(urlText)
+		return url.get_attribute("href");
+		
+		
 
 try:
-#	print dir(driver)
-
-	baseUrl = "http://rc.fizlesh.ru/"
-	driver.get(baseUrl + "/qqq")
-
-	# expect error page
-	errorDiv = driver.find_element_by_xpath("//div[@class='error-widget']")
-
-	#print errorDiv.text
-
-	if u"Нет такой страницы" not in errorDiv.text:
-		raise RuntimeError("Wrong page opened on non-existing URL")
+	test = SeleniumTest()
 	
-	homeUrl = driver.find_element_by_link_text("этой ссылке")
+	test.page("/qqq");
 	
-	#print dir(homeUrl)
+	test.assertTextPresent("//div[@class='error-widget']", u"Нет такой страницы")
 	
-	homeHref = homeUrl.get_attribute("href")
-	print "home ref on 404 page: ", homeHref
+	homeHref = test.getUrlByLinkText(u"этой ссылке")
 	
-	driver.get(homeHref)
-
-	print sys.argv[0], "PASSED"
-	driver.close()
+	print "Home reference on 404 page: ", homeHref
+	
+	test.site(homeHref)
 	
 except Exception as e:
-	if debugMode: driver.close()
-	print "Test failed: ", e
+	#if debugMode: driver.close()
+	print "TEST FAILED: ", e
 	traceback.print_exc(1)
+	sys.exit(1)
     
