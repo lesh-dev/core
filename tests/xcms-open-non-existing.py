3b56e33f73747548d956045681f27bfdc4343f21
Dmitry Veltishev <vdm-photo@ya.ru>
2012-10-18 22:37:43 +0400

    2-nd test re-written to webdriver engine

--- tests/xcms-open-non-existing.py
+++ tests/xcms-open-non-existing.py
@@ -4,70 +4,25 @@
 from selenium import webdriver
 import os, sys, traceback
 
+import selenium_test
+
 debugMode = True #"TRUE" in os.getenv("XCMS_TEST_DEBUG");
 
-class SeleniumTest:
-	def __init__(self, baseUrl = None):
-#		print "Init SeleniumTest"
-		if baseUrl is None:
-			if len(sys.argv) < 2:
-				raise RuntimeError("Base URL for test is not set. ")
-			else:
-				baseUrl = sys.argv[1];
-				
-		self.m_driver = webdriver.Firefox()
-		self.m_baseUrl = baseUrl;
-		
-	def __del__(self):
-#		print "Destructing SeleniumTest"
-		if hasattr(self, 'm_driver'):
-			self.m_driver.close()
-		
-	def page(self, url):
-		
-		# detect if URL is full
-		fullUrl = self.m_baseUrl + url
-			
-		print "Navigating to " + fullUrl
-		self.m_driver.get(fullUrl);
-	
-	def site(self, fullUrl):
-		print "Going to site " + fullUrl
-		self.m_driver.get(fullUrl)
-		
-	def drv(self):
-		return self.m_driver;
-		
-	def checkTextPresent(self, xpath, text):
-		ele = self.m_driver.find_element_by_xpath(xpath)
-		return text in ele.text;
-		
-	def assertTextPresent(self, xpath, text):
-		if not self.checkTextPresent(xpath, text):
-			raise RuntimeError("Text '" + text + "' not appers on page in element " + xpath)
-			
-	def getUrlByLinkText(self, urlText):
-		url = self.m_driver.find_element_by_link_text(urlText)
-		return url.get_attribute("href");
-		
-		
-
 try:
-	test = SeleniumTest()
-	
-	test.page("/qqq");
-	
+	test = selenium_test.SeleniumTest()
+	test.gotoPage("/qqq");
 	test.assertTextPresent("//div[@class='error-widget']", u"Нет такой страницы")
-	
 	homeHref = test.getUrlByLinkText(u"этой ссылке")
-	
 	print "Home reference on 404 page: ", homeHref
 	
-	test.site(homeHref)
+	test.gotoSite(homeHref)
 	
-except Exception as e:
-	#if debugMode: driver.close()
+except RuntimeError as e:
 	print "TEST FAILED: ", e
-	traceback.print_exc(1)
+	print "Last step: ", traceback.print_exc(1)
 	sys.exit(1)
+except Exception as e:
+	print "TEST ERROR: ", e
+	traceback.print_exc()
+	sys.exit(2)
     
