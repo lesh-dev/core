881a1fb711c7e39e18f5c89ca96a5813ea1f2a0e
doctor <trousev@yandex.ru>
2012-10-21 04:53:59 +0400

    1. Damm, I've already commited deprecated scripts deletion, WTH? 2. publishtools are ready to forum movement

--- tools/publish/deprecated/res/settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $content_dir = "content/";
-    $design_dir = "fizlesh.ru-design/";
-    $engine_dir = "engine/";
-    $engine_pub = "engine_public/";
-    $web_prefix = "";
-    $mailer_enabled = true;
-?>
