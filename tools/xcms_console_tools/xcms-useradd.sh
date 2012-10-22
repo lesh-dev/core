0d01ca02be70dd5901aaa23ff4dddbfca1049b76
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2012-10-22 14:58:50 +0400

    Code styling in xcms-useradd.sh, long args.
    * Fixed code style in useradd script
    * Added long human-readable arguments
    * Added nice help (-h, --help)

--- tools/xcms_console_tools/xcms-useradd.sh
+++ tools/xcms_console_tools/xcms-useradd.sh
@@ -1,49 +1,78 @@
 #!/bin/bash
-if [ $1. == . ]; then
-    echo "$0 [-x xcms_home] [-u username] [-p password] user_email@example.com ";
+
+print_usage()
+{
+    cat <<EOF
+Usage: $0 <options> <user-email>"
+    Options:
+        -x, --xcms-home  XCMS Home
+        -u, --user       User name
+        -p, --password   User password
+        -h, --help       This help
+    Example:
+        $0 -u ivanov -p i4OO-lcf7 ivanov.leonid@gmail.com
+EOF
+}
+
+error_exit()
+{
+    msg="$1"
+    if [ -n "Error: $msg" ] ; then
+        echo "$msg"
+    fi
+    echo
+    print_usage
+}
+
+if [ -z "$1" ]; then
+    print_usage
     exit 1
 fi
 while [ true ]; do
-  if [ $1. == . ]; then
-    break;
-  fi
-  if [ $1. == "-x". ]; then
-    shift
-    XCMS_HOME=$1
-    shift
-    continue
-  fi
-  if [ $1. == "-p". ]; then
-    shift
-    password=$1
-    shift
-    continue
-  fi
-  if [ $1. == "-u". ]; then
-    shift
-    login=$1
-    shift
-    continue
-  fi
-  break 
+    if [ -z "$1" ]; then
+        break
+    fi
+    if [ "$1" == "-x" ] || [ "$1" == "--xcms-home" ]; then
+        shift
+        XCMS_HOME=$1
+        shift
+        continue
+    fi
+    if [ "$1" == "-p" ] || [ "$1" == "--password" ]; then
+        shift
+        password=$1
+        shift
+        continue
+    fi
+    if [ "$1" == "-u" ] || [ "$1" == "--user" ] ; then
+        shift
+        login=$1
+        shift
+        continue
+    fi
+    if [ "$1" == "-h" ] || [ "$1" == "--help" ] ; then
+        print_usage
+        exit 1
+    fi
+    break
 done
 mail=$1
-if [ $mail. == . ]; then
-  echo "No mail specified"
-  exit 1
+if [ -z "$mail" ]; then
+    error_exit "No mail specified. "
 fi
-if [ $XCMS_HOME. == . ]; then
-  echo "No XCMS_HOME specified. Abort."
-  exit 1
+if [ -z "$XCMS_HOME" ]; then
+    error_exit "No \$XCMS_HOME specified. Either set XCMS_HOME environment or set it from command line. "
 fi
-if [ $login. == . ]; then
-  login=$(echo $mail | sed s/"@.*"//g )
-  echo "User's login: $login"
+if [ -z "$login" ]; then
+    login=$(echo $mail | sed 's/@.*//g' )
+    echo "Login is not set. Autogenerating from email: '$login'"
+    if [ -z "$login" ] ; then
+        error_exit "Login autogenerating from email failed. Please check email format. "
+    fi
 fi
-if [ $password. == . ]; then
-  password=$(echo "$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM" | md5sum | cut -c2-9)
-  echo "Password: $password"
+if [ -z "$password" ]; then
+    password=$(echo "$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM" | md5sum | cut -c2-9)
+    echo "Password: $password"
 fi
 
 sudo su www-data -c "php $(dirname $0)/xcms.php  basedir=$XCMS_HOME useradd login=$login password=$password mail=$mail groups=editor,ank notify=yes"
-
