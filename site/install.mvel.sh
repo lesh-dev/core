f678b6c5b1a1cadda0af2ab520987ce5dc6c03e8
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2013-02-12 07:48:08 +0400

    Some script fixes

--- site/install.mvel.sh
+++ site/install.mvel.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -xe
 
-dest="/var/www/html/site"
+dest_name="site"
+if [ "$HOSTNAME" == "blackbox" ] ; then
+    dest_name="lesh"
+fi
+dest="/var/www/html/$dest_name"
 
 if [ -z "$dest" ] ; then
     echo "Destination path cannot be empty"
@@ -15,11 +19,11 @@ sqlite3 $db < engine/dbpatches/dbinit-v2.sql
 #sqlite3 $db < unused-dbinit.sql
 #sqlite3 $db < ../junk/$content_name/ank/anketas-2012.07.23.sql
 #cp ../junk/$content_name/ank/lesh-2012.08.30.sqlite3 $db
-#cp /var/www/html/site/$content_name/ank/fizlesh.sqlite3 complete.sql
+#cp /var/www/html/$dest_name/$content_name/ank/fizlesh.sqlite3 complete.sql
 #cp complete.sql $db
 
 sudo mkdir -p "$dest"
-sudo rm -rf /var/www/html/site/*
+sudo rm -rf /var/www/html/$dest_name/*
 sudo cp -a * "$dest/"
 sudo mkdir -p "$dest/$content_name"
 sudo cp -a ../../content-fizlesh.ru/content/* "$dest/$content_name/"
