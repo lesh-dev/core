1cda8034839019538000e0e568641b272b5ae3b6
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2013-04-24 02:14:56 +0400

    apache -> www-data user name

--- tools/xcms_console_tools/xcms-deliver-notifications.sh
+++ tools/xcms_console_tools/xcms-deliver-notifications.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 XCMS_HOME="/srv/www/production"
-su apache -c "php /srv/tools/git-working-copy/tools/xcms_console_tools/xcms.php 'basedir=$XCMS_HOME' deliver-notifications 'mail-group=reg'"
+su www-data -c "php /srv/tools/git-working-copy/tools/xcms_console_tools/xcms.php 'basedir=$XCMS_HOME' deliver-notifications 'mail-group=reg'"
