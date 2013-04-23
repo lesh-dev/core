b5cfe2f2af4be0d0fb0b8ed25cb23d121f999fdf
Dmitry Veltishev <vdm-photo@ya.ru>
2013-04-24 01:53:46 +0400

    test suite generator simplified (py version)

--- tests/generate-test-set.py
+++ tests/generate-test-set.py
@@ -4,37 +4,35 @@ import re
 from os import listdir
 from os.path import isfile, join
 
-py_files = sorted([ f for f in listdir('.') if isfile(f) and f[-3:] == '.py' ])
+pyFiles = sorted([ f for f in listdir('.') if isfile(f) and f[-3:] == '.py' ])
 
 imp = []
 main = []
 
-for fn in py_files:
-    fnc = fn[:-3]
+for fn in pyFiles:
+	moduleName = fn[:-3]
 
-    cl = ''
-    for l in open(fn, 'r'):
-        if 'class Xcms' in l:
-            cl = l
-            break
-    cl = cl.strip()
-    r = re.match("class ([A-Za-z]+)\(SeleniumTest\):", cl)
-    if not r:
-        #print "Cannot match in ", cl
-        continue
+	classLineList = filter(lambda x: "class Xcms" in x, [line for line in open(fn, 'r')])
+	if not classLineList:
+		continue
+	classLine = classLineList.pop().strip()
+	r = re.match("class ([\w]+)\(SeleniumTest\):", classLine)
+	if not r:
+		#print "Cannot match in ", cl
+		continue
 
-    imp.append(fnc)
-    main.append( '"%s": %s.%s(baseUrl, args),' % (fn, fnc, r.group(1)))
+	imp.append(moduleName)
+	main.append( '"%s": %s.%s(baseUrl, args),' % (fn, moduleName, r.group(1)))
 
 print '#!/usr/bin/python'
 print
 print "# This file is AUTO-GENERATED. Do not edit it directly, edit generator instead"
 print
 for i in imp:
-    print 'import', i
+	print 'import', i
 print
 
 print 'def getTests(baseUrl, args): return {'
 for i in main:
-    print '    ' + i
+	print '    ' + i
 print '}'
