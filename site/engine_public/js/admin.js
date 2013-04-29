f454776a4b4209b87cd7c13ba0c63719728811df
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2013-04-30 03:10:06 +0400

    Fixed #560

--- site/engine_public/js/admin.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(
-    function() {
-        $('#editor-help').hide();
-        $('#show-editor-help').click(
-            function() {
-                $('#editor-help').slideToggle(400);
-                return false;
-            });
-    });
