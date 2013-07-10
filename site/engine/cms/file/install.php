1f07a32ef023229f1973892fda603e03cfda458f
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2013-07-10 22:58:55 +0400

    Obey code style and English language.

--- site/engine/cms/file/install.php
+++ site/engine/cms/file/install.php
@@ -1,5 +1,5 @@
 <?php
     $LIST["installed"] = "ok";
-    $LIST["immidiate"] = "false";
-    xcms_write("{$SETTINGS["datadir"]}cms/pages/$pageid/lockmenu", "true");
+    $LIST["immediate"] = "no";
+    $LIST["menu-locked"] = "yes";
 ?>
