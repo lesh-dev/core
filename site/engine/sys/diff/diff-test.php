e15c3a58ef78df395ca0ae2038a283dad2ab081e
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2013-08-19 19:36:30 +0400

    Diff postprocessing.

--- site/engine/sys/diff/diff-test.php
+++ site/engine/sys/diff/diff-test.php
@@ -1,29 +1,61 @@
 <?php
     require_once 'finediff.php';
 
-    function callbackFunc($code, $from, $from_offset, $n)
-    {
-        print_r("callbackFunc called\n");
-        print_r("$code\n");
-        print_r("from: $from\n");
-        print_r("from_ofs: $from_offset\n");
-        print_r("count: $n\n");
-        print_r("------------------------\n");
+    define('CONTEXT_SIZE', 2);
 
+    function diff_postprocess($diff)
+    {
+        $output = "";
+
+        $ad = explode("\n", $diff);
+        $show = array();
+        for ($i = 0; $i < count($ad); ++$i)
+            $show[$i] = false;
+
+        for ($i = 0; $i < count($ad); ++$i)
+        {
+            $ln = $ad[$i];
+            if (strpos($ln, "<ins>") !== false ||
+                strpos($ln, "<del>") !== false)
+            {
+                for ($j = $i - CONTEXT_SIZE; $j <= $i + CONTEXT_SIZE; ++$j)
+                    if ($j >= 0 && $j < count($show))
+                        $show[$j] = true;
+
+            }
+        }
+        $dots = false;
+        for ($i = 0; $i < count($show); ++$i)
+        {
+            $ln = $ad[$i];
+            if ($show[$i])
+            {
+                $ln = str_replace("\n", "", $ln);
+                $output .= "<div>$ln</div>\n";
+                $dots = false;
+            }
+            else
+            {
+                if ($dots)
+                    continue;
+                $dots = true;
+                $output .= "<div style=\"color: #7f7f7f;\">***</div>\n";
+            }
+        }
+        return $output;
     }
 
     $from_text = file_get_contents("text.html");
     $to_text = file_get_contents("newtext.html");
 
-    //$from_text = mb_convert_encoding($from_text, 'HTML-ENTITIES', 'UTF-8');
-    //$to_text = mb_convert_encoding($to_text, 'HTML-ENTITIES', 'UTF-8');
-
-    //$opcodes = FineDiff::getDiffOpcodes($from_text, $to_text, FineDiff::$wordGranularity);
+    //$opcodes = FineDiff::getDiffOpcodes($from_text, $to_text, FineDiff::$characterGranularity);
     $opcodes = FineDiff::getDiffOpcodes($from_text, $to_text, FineDiff::$wordGranularity);
 
-    //print_r("codes|||$opcodes|||\n");
     $diff_html = FineDiff::renderDiffToHTMLFromOpcodes($from_text, $opcodes);
     echo "<style>
+        body {
+            font-family: monospace;
+        }
         ins {
             color: #009f00;
             text-decoration: none;
@@ -32,5 +64,7 @@
             color: #9f0000;
         }
     </style>";
+
+    $diff_html = diff_postprocess($diff_html);
     echo $diff_html;
 ?>
