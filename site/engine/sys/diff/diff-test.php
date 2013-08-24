155d14b88e2c6df453b11c57c65bb6ffbffd0d1e
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2013-08-24 19:07:17 +0400

    Diff test turned into diff-helper.

--- site/engine/sys/diff/diff-test.php
+++ site/engine/sys/diff/diff-test.php
@@ -3,7 +3,10 @@
 
     define('CONTEXT_SIZE', 2);
 
-    function diff_postprocess($diff)
+    /**
+      * Diff postprocessing: markup with styles
+      **/
+    function xcms_diff_postprocess($diff)
     {
         $output = "";
 
@@ -39,32 +42,32 @@
                 if ($dots)
                     continue;
                 $dots = true;
-                $output .= "<div style=\"color: #7f7f7f;\">***</div>\n";
+                $output .= '<div style="color: #7f7f7f;">***</div>'."\n";
             }
         }
+        $output = str_replace('<ins>', '<ins style="color: #009f00; text-decoration: none;">', $output);
+        $output = str_replace('<del>', '<del style="color: #9f0000;">', $output);
         return $output;
     }
 
-    $from_text = file_get_contents("text.html");
-    $to_text = file_get_contents("newtext.html");
+    /**
+      * Make html-based diff from text
+      **/
+    function xcms_diff_html($from_text, $to_text)
+    {
+        $opcodes = FineDiff::getDiffOpcodes($from_text, $to_text, FineDiff::$wordGranularity /* FineDiff::$characterGranularity */);
+        $diff_html = FineDiff::renderDiffToHTMLFromOpcodes($from_text, $opcodes);
+        $diff_html = xcms_diff_postprocess($diff_html);
+    }
 
-    //$opcodes = FineDiff::getDiffOpcodes($from_text, $to_text, FineDiff::$characterGranularity);
-    $opcodes = FineDiff::getDiffOpcodes($from_text, $to_text, FineDiff::$wordGranularity);
+    // Test
+    function xcms_diff_test()
+    {
+        $from_text = file_get_contents("text.html");
+        $to_text = file_get_contents("newtext.html");
+        $diff_html = xcms_diff_html($from_text, $to_text);
+        echo $diff_html;
+    }
 
-    $diff_html = FineDiff::renderDiffToHTMLFromOpcodes($from_text, $opcodes);
-    echo "<style>
-        body {
-            font-family: monospace;
-        }
-        ins {
-            color: #009f00;
-            text-decoration: none;
-        }
-        del {
-            color: #9f0000;
-        }
-    </style>";
-
-    $diff_html = diff_postprocess($diff_html);
-    echo $diff_html;
+    xcms_diff_test();
 ?>
