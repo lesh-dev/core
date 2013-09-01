6713e26e00969d707c006a2ce3d05374f9bebb2d
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2013-09-02 00:50:03 +0400

    Improved converter script

--- tools/xsm-merge/convert-news.sh
+++ tools/xsm-merge/convert-news.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+
+set -xe
+
+(cd news && ls -1 *.news > ../news-list.txt && cd .. )
 rm -rf ./new-news
 mkdir -p ./new-news
-php news-to-contlist.php news-list.txt
+cp news/template new-news/
+php ./news-to-contlist.php ./news-list.txt
