4fab0403c5b542c71f037b12256f1d362984809e
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2013-09-05 01:09:36 +0400

    Removed obsolete conversion script used for debugging.

--- tools/xsm-merge/convert-news2.4.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -xe
-
-(cd news && ls -1 *.news > ../news-list.txt && cd .. )
-rm -rf ./new-news
-mkdir -p ./new-news
-cp news/template new-news/
-php ./news-to-contlist.php ./news-list.txt
