fc4f1d884023a00c72cf3612fa154272892c5b20
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2013-09-04 11:06:16 +0400

    Almost working conversion script.

--- tools/xsm-merge/migrate2.4.sh
+++ tools/xsm-merge/migrate2.4.sh
@@ -21,3 +21,28 @@ sudo cp settings2.4.php settings.php
 sudo sed -i -e "s/@@CONTENT@/$content_dir/" settings.php
 sudo php merger-v2.3-to-v2.4.php
 sudo cp fizlesh.sqlite3 $target_db
+
+news_path="$site_root/$content_dir/cms/pages/z01News"
+ls $news_path
+sudo cp -r "$news_path" "$news_path.backup"
+sudo rm -f "$news_path"/*.gz
+sudo bash -xe <<EOF
+    ls -1 $news_path/*.news > news-list.txt
+EOF
+sudo bash -xe <<EOF
+    cat > $news_path/info <<INFO
+owner:root
+type:contlist
+view:#all
+header:Новости
+subheader:
+edit:#editor
+menu-title:Новости
+menu-hidden:
+menu-auth-only:
+INFO
+    # remove old metainfo
+    rm -f $news_path/_*
+    # remove unused template
+    rm -f $news_path/template
+EOF
