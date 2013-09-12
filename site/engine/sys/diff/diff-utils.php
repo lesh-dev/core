1181b13af5aac7ee77f4ba99e90640d1f8812fba
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2013-09-13 01:06:25 +0400

    Re-fixed #528 (at least, majority of problems).

--- site/engine/sys/diff/diff-utils.php
+++ site/engine/sys/diff/diff-utils.php
@@ -8,44 +8,8 @@
       **/
     function xcms_diff_postprocess($diff)
     {
-        $output = "";
-
-        $ad = explode(EXP_LF, $diff);
-        $show = array();
-        for ($i = 0; $i < count($ad); ++$i)
-            $show[$i] = false;
-
-        for ($i = 0; $i < count($ad); ++$i)
-        {
-            $ln = $ad[$i];
-            if (strpos($ln, "<ins>") !== false ||
-                strpos($ln, "<del>") !== false)
-            {
-                for ($j = $i - CONTEXT_SIZE; $j <= $i + CONTEXT_SIZE; ++$j)
-                    if ($j >= 0 && $j < count($show))
-                        $show[$j] = true;
-
-            }
-        }
-        $dots = false;
-        for ($i = 0; $i < count($show); ++$i)
-        {
-            $ln = $ad[$i];
-            if ($show[$i])
-            {
-                $ln = str_replace("\n", "", $ln);
-                $ln = str_replace("\r", "", $ln);
-                $output .= "<div>$ln</div>\n";
-                $dots = false;
-            }
-            else
-            {
-                if ($dots)
-                    continue;
-                $dots = true;
-                $output .= '<div style="color: #7f7f7f;">***</div>'."\n";
-            }
-        }
+        $output = str_replace(EXP_LF, '<br/>', $diff);
+        $output = str_replace('<skip/>', '<span style="color: #7f7f7f;">***</span>', $output);
         $output = str_replace('<ins>', '<ins style="color: #009f00; text-decoration: none;">', $output);
         $output = str_replace('<del>', '<del style="color: #9f0000;">', $output);
         return $output;
@@ -57,7 +21,7 @@
     function xcms_diff_html($from_text, $to_text)
     {
         $opcodes = FineDiff::getDiffOpcodes($from_text, $to_text, FineDiff::$wordGranularity /* FineDiff::$characterGranularity */);
-        $diff_html = FineDiff::renderDiffToHTMLFromOpcodes($from_text, $opcodes);
+        $diff_html = FineDiff::renderDiffToHTMLFromOpcodesContext($from_text, $opcodes);
         $diff_html = xcms_diff_postprocess($diff_html);
         return $diff_html;
     }
@@ -65,8 +29,9 @@
     // Test
     function xcms_diff_test()
     {
-        $from_text = file_get_contents("text.html");
-        $to_text = file_get_contents("newtext.html");
+        global $engine_dir;
+        $from_text = file_get_contents("$engine_dir/sys/diff/text.html");
+        $to_text = file_get_contents("$engine_dir/sys/diff/newtext.html");
         $diff_html = xcms_diff_html($from_text, $to_text);
         echo $diff_html;
     }
