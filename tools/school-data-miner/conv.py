fc6f511c6c77d8fe019c8270eed273ff969bdce2
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2013-10-17 11:56:47 +0400

    Added cities for miner.

--- tools/school-data-miner/conv.py
+++ tools/school-data-miner/conv.py
@@ -9,35 +9,38 @@ import urllib
 import re
 
 def grab_director(city, school_num, redir_url = None):
+    if school_num and not re.match('[0-9]+', school_num):
+        return None
+
     if city:
         url = 'http://www.proshkolu.ru/org/' + city + '-' + school_num + '/'
     else:
         url = redir_url
 
-    print url
+    #print url
     prog = re.compile("location.href='(.+?)&")
     dp = re.compile('boss/.>(.*?)<')
     f = urllib.urlopen(url)
     contents = f.read()
     if 'location.href' in contents:
-        print 'GOT REDIRECT'
+        #print 'GOT REDIRECT'
         res = prog.search(contents)
         if res:
-            print res.group(1)
+            #print res.group(1)
             return grab_director(city=None, school_num=None, redir_url=res.group(1))
         else:
-            print "-------- NOT MATCHED"
+            #print "-------- NOT MATCHED"
             return None
 
     d = u'Директор'.encode('cp1251')
     boss = None
     res = dp.search(contents)
     if res:
-        boss = res.group(0)
-        print boss
+        boss = res.group(1).decode('cp1251')
+        #print boss
         return boss
     else:
-        print ' ### BOSS NOT FOUND'
+        #print ' ### BOSS NOT FOUND'
         return None
 
 
@@ -66,8 +69,15 @@ with open('schools.csv', 'r') as f:
         name = name.replace(u'СОШ', '').strip()
         email = email.lower().strip()
 
+        fio = 'D'
         if city == u'Кострома':
-            fio = grab_director(city='kostroma', school_num=name)
+            fio = grab_director(city='kostroma', school_num=name) or 'N'
+        elif city == u'Саратов':
+            fio = grab_director(city='saratov', school_num=name) or 'N'
+        elif city == u'Ижевск':
+            fio = grab_director(city='izhevsk', school_num=name) or 'N'
+        elif city == u'Киров':
+            fio = grab_director(city='kirov', school_num=name) or 'N'
 
-        print school_type, '|', name.encode('utf-8'), '|', city.encode('utf-8'), '|', email.encode('utf-8')
+        print school_type, '|', name.encode('utf-8'), '|', city.encode('utf-8'), '|', fio.encode('utf-8') + ' <' + email.encode('utf-8') + '>'
 
