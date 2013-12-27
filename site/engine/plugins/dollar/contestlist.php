d2a6709137ac1a00b6cf86ff51ca6027ec7c5ac3
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2013-12-27 05:49:01 +0400

    Added forgotten file

---
+++ site/engine/plugins/dollar/contestlist.php
@@ -0,0 +1,31 @@
+<?php
+include("$engine_dir/contest/scheme.php");
+include("$engine_dir/contest/action.php");
+
+// redefine it
+$CONTEST_CURRENT_YEAR = $_0;
+$FILTER = "( contest_year = \"$CONTEST_CURRENT_YEAR\" )";
+
+$probs = xdb_get_table("problems", NULL, $FILTER);
+$works = ctx_get_works();
+
+$results = ctx_calculate_results($works, $probs);
+
+$done = $results['done'];
+$done_sum = $results['done_sum'];
+
+echo count($done);
+
+?>
+<table>
+<thead><tr><tr><th>#</th><th>Имя</th><th>Результат</th></tr>
+<?php
+$i = 1;
+foreach ($done_sum as $sum)
+    foreach ($done[$sum] as $work)
+    {
+        echo "<tr><td>$i</td>".ctx_print_result_row($work, $probs, true)."</tr>\n";
+        $i++;
+    }
+?></table><?php
+?>
