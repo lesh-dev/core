87ed52775579fe81c7f7f33bb7b8b70c0b62c685
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2013-12-29 15:03:50 +0400

    Remove dead code. Rename 'userlist' to 'user_manage' by code style

--- tests/xcms_auth_change_user_by_admin.py
+++ tests/xcms_auth_change_user_by_admin.py
@@ -9,7 +9,7 @@ class XcmsAuthChangeUserByAdmin(xtest_common.XcmsTest):
     Add new user and change it's profile by admin (root).
     Then see if proper menu items appear after user login.
     """
-            
+
     def run(self):
         # first, login as admin
         inpLogin = "priv_user_" + random_crap.randomText(8)
@@ -18,67 +18,67 @@ class XcmsAuthChangeUserByAdmin(xtest_common.XcmsTest):
         inpName = u"Саша Тестов" + random_crap.randomText(6)
 
         inpLogin, inpEMail, inpPass, inpName = self.createNewUser(inpLogin, inpEMail, inpPass, inpName)
-        
+
         print "logging as created user. "
         if not self.performLogin(inpLogin, inpPass):
             self.failTest("Cannot login as newly created user. ")
-        
-        
+
+
         self.assertUrlNotPresent(self.getAdminPanelLinkName(), "default created user should have no Admin rights. ")
         self.assertUrlNotPresent(self.getEditPageInPlaceLinkName(), "default created user should have no Editor rights. ")
         self.assertUrlNotPresent(self.getAnketaListMenuName(), "default created user should have no Manager rights. ")
         print "Okay, no we see that default user is not Admin and not Editor. "
-        
-        # logout self 
+
+        # logout self
         self.performLogoutFromSite()
 
         # login as admin, enter user profile and change some fields.
-        
+
         self.performLoginAsAdmin()
-    
+
         self.gotoAdminPanel()
         self.gotoUserList()
-            
+
         print "enter user profile in admin CP"
 
         self.gotoUrlByPartialLinkText(inpLogin)
 
         self.assertElementValueById("name-input", inpName)
         self.assertElementValueById("email-input", inpEMail)
-        
+
         inpName = inpName + "_changed"
-        
+
         inpName = self.fillElementById("name-input", inpName)
         print "New user name: ", inpName
-        
+
         print "Check if administrator priviledge is off now"
         self.assertCheckboxValueById("group_#admin", False)
-        
+
         print "Add administrator priviledge"
         self.clickElementById("group_#admin")
 
         self.clickElementById("update_user")
-        
+
         self.performLogoutFromAdminPanel()
-        
+
         print "logging as new user with changed permissions. now he is Admin. "
         if not self.performLogin(inpLogin, inpPass):
             self.failTest("Cannot login again as newly created user (with admin privs). ")
-        
+
         self.assertUrlPresent(self.getAdminPanelLinkName(), "Now user should have Admin priviledges. ")
         self.performLogoutFromSite()
-        
+
         # ---------------------- 2nd stage: Editor
-        
+
         self.performLoginAsAdmin()
-        
+
         self.gotoAdminPanel()
         self.gotoUserList()
-            
+
         print "goto user profile in admin CP"
 
         self.gotoUrlByPartialLinkText(inpLogin)
-        
+
         self.assertCheckboxValueById("group_#admin", True)
         self.assertCheckboxValueById("group_#editor", False)
 
@@ -88,43 +88,43 @@ class XcmsAuthChangeUserByAdmin(xtest_common.XcmsTest):
         self.clickElementById("group_#editor")
         self.assertCheckboxValueById("group_#admin", False)
         self.assertCheckboxValueById("group_#editor", True)
-        
+
         self.clickElementById("update_user")
-        
+
         self.performLogoutFromAdminPanel()
-        
+
         print "logging as new user with 2-nd time changed permissions. now he is Editor. "
         if not self.performLogin(inpLogin, inpPass):
             self.failTest("Cannot login again as newly created user (with Editor privs). ")
-        
+
         self.assertUrlPresent(self.getAdminPanelLinkName(), "Now our user should have no Admin rights, but Editor uses admin panel. ")
         self.assertUrlNotPresent(self.getAnketaListMenuName(), "Our user still have no Manager rights. ")
         self.assertUrlPresent(self.getEditPageInPlaceLinkName(), "Now our user should have Editor rights. ")
-        
+
         self.gotoAdminPanel()
         self.assertUrlNotPresent(self.getUserListLinkName(), "Editor should not see 'Users' menu. ")
-        
+
         accessDeniedMsg = u"Доступ запрещён"
-        
-        self.gotoPage("/?&mode=userlist&page=index&ref=ladmin")
+
+        self.gotoPage("/?&mode=user_manage&page=index&ref=ladmin")
         self.assertBodyTextPresent(accessDeniedMsg, "Hack of 'users' hidden link succeeded. ")
-        
+
         self.assertUrlNotPresent(u"Группы", "Editor should not see 'Groups' menu. ")
         self.gotoPage("/?&mode=group_admin&page=index&ref=ladmin")
-        self.assertBodyTextPresent(accessDeniedMsg, "Hack of 'groups' hidden link succeeded. ")        
-        
+        self.assertBodyTextPresent(accessDeniedMsg, "Hack of 'groups' hidden link succeeded. ")
+
         self.performLogoutFromAdminPanel()
 
         # -------------------------------------- 3 stage: anketa manager
         self.performLoginAsAdmin()
-        
+
         self.gotoAdminPanel()
         self.gotoUserList()
-            
+
         print "goto user profile in admin CP"
 
         self.gotoUrlByPartialLinkText(inpLogin)
-        
+
         self.assertCheckboxValueById("group_#admin", False)
         self.assertCheckboxValueById("group_#editor", True)
         self.assertCheckboxValueById("group_#ank", False)
@@ -136,17 +136,17 @@ class XcmsAuthChangeUserByAdmin(xtest_common.XcmsTest):
         self.assertCheckboxValueById("group_#admin", False)
         self.assertCheckboxValueById("group_#editor", False)
         self.assertCheckboxValueById("group_#ank", True)
-        
+
         self.clickElementById("update_user")
-        
+
         self.performLogoutFromAdminPanel()
-        
+
         print "logging as new user with 3-rd time changed permissions. now he is Manager. "
         if not self.performLogin(inpLogin, inpPass):
             self.failTest("Cannot login again as newly created user (with Manager privs). ")
-        
+
         self.assertUrlNotPresent(self.getAdminPanelLinkName(), "Now our user should have no access to Admin panel. ")
         self.assertUrlPresent(self.getAnketaListMenuName(), "Our user should now obtain Manager rights. ")
         self.assertUrlNotPresent(self.getEditPageInPlaceLinkName(), "On third stage, our user should have no Editor rights. ")
         self.performLogoutFromSite()
-        
+
