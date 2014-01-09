e9cd2c25a10bf584c08566200f5f4edced062174
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2014-01-09 11:16:47 +0400

    Fixed #233

--- site/index.php
+++ site/index.php
@@ -8,19 +8,37 @@
     }
     header("Content-Type: text/html; charset=utf-8");
     date_default_timezone_set('Europe/Moscow');
+
+    if (!file_exists("settings.php"))
+    {?>
+        <html>
+            <head>
+                <title>XCMS Configuration Error</title>
+            </head>
+            <body>
+                <h1>XCMS Configuration Error</h1>
+                <p>Cannot find configuration (<tt>settings.php</tt>). You must either
+                run installer to generate it or write it manually.
+                To run installer just rename <tt>install.nt.php</tt>
+                to <tt>install.php</tt> and refresh current page.</p>
+            </body>
+        </html><?php
+        die();
+    }
     require_once ("settings.php");
-    require_once ("$engine_dir/sys/settings.php");
-    require_once ("$engine_dir/sys/string.php");
-    require_once ("$engine_dir/sys/tag.php");
-    require_once ("$engine_dir/sys/cms.php");
-    require_once ("$engine_dir/sys/file.php");
-    require_once ("$engine_dir/sys/util.php");
-    require_once ("$engine_dir/sys/unittest.php");
-    require_once ("$engine_dir/sys/auth.php");
-    require_once ("$engine_dir/sys/logger.php");
-    require_once ("$engine_dir/sys/compiler.php");
-    require_once ("$engine_dir/sys/mailer.php");
-    require_once ("$engine_dir/sys/resample.php");
+
+    require_once ("${engine_dir}sys/settings.php");
+    require_once ("${engine_dir}sys/string.php");
+    require_once ("${engine_dir}sys/tag.php");
+    require_once ("${engine_dir}sys/cms.php");
+    require_once ("${engine_dir}sys/file.php");
+    require_once ("${engine_dir}sys/util.php");
+    require_once ("${engine_dir}sys/unittest.php");
+    require_once ("${engine_dir}sys/auth.php");
+    require_once ("${engine_dir}sys/logger.php");
+    require_once ("${engine_dir}sys/compiler.php");
+    require_once ("${engine_dir}sys/mailer.php");
+    require_once ("${engine_dir}sys/resample.php");
     if (!$session_result)
         xcms_log("Session start failed");
 
