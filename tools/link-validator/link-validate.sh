b2f87813c6f340c65393e06af8304d33bc925f7b
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2014-01-15 00:35:27 +0400

    Fix code style

--- tools/link-validator/link-validate.sh
+++ tools/link-validator/link-validate.sh
@@ -4,9 +4,9 @@
 # maintainer: vdm (vdm-photo@ya.ru)
 
 if ! [ "$1" ]; then
-	echo "This script validates specified site and checks link integrity."
-	echo "Syntax: $0 <url-to-validate>"
-	exit 1
+    echo "This script validates specified site and checks link integrity."
+    echo "Syntax: $0 <url-to-validate>"
+    exit 1
 fi
 
 URL="$1"
@@ -15,10 +15,10 @@ BROKEN="broken.log"
 wget --exclude-directories="/forum" --no-verbose --spider -r $URL -o "$LOG"
 
 if grep -B2 "broken link!" "$LOG" > "$BROKEN"; then
-	echo "ERROR! Broken links detected! See files '$BROKEN' and '$LOG' for details."
-	exit 1
+    echo "ERROR! Broken links detected! See files '$BROKEN' and '$LOG' for details."
+    exit 1
 else
-	echo "No broken links found"
-	rm -f "$BROKEN"
-	exit 0
+    echo "No broken links found"
+    rm -f "$BROKEN"
+    exit 0
 fi
