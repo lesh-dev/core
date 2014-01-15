a6af163c5c5f7eb59979a114aec11fc50b81ed45
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2014-01-15 09:12:03 +0400

    Fix description and code style

--- tools/content-archive-rotate/content-archive-rotate.sh
+++ tools/content-archive-rotate/content-archive-rotate.sh
@@ -1,36 +1,39 @@
-#!/bin/bash
-# this helper script lists site content and tag for each page.
+#!/usr/bin/env bash
 
-if ! [ "$1" ]; then
-	echo "Syntax: $0 <directory-to-clean-versions> <versions-to-keep>"
-	exit 1
+# cleans old page versions
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <directory-to-clean-versions> [versions-to-keep]"
+    exit 1
 fi
 
 SEARCH_DIR="`cd $1; pwd`"
 
-shift;
+shift
 
 ARC_VERSIONS=5
 
-if [ "$1" ]; then
-	ARC_VERSIONS="$1"
+if [ -n "$1" ]; then
+    ARC_VERSIONS="$1"
 fi
 
 echo "Cleaning versions in $SEARCH_DIR"
 
 function cleanup_dir()
 {
-	if ! [ -r "$DIR/content" ]; then
-		echo "Directory $DIR has no content, skipping. "
-		return 0;
-	fi
-	if ! ls $DIR/content-version*gz &>/dev/null; then return 0; fi
-	echo "Cleaning version in directory $DIR"
-#	ls -1t $DIR/content-version*gz
-	ls -1t $DIR/content-version*gz | head -n "-$ARC_VERSIONS" | xargs rm -f
+    if ! [ -r "$DIR/content" ]; then
+        echo "Directory $DIR has no content, skipping. "
+        return 0
+    fi
+    if ! ls $DIR/content-version*gz &>/dev/null; then
+        return 0
+    fi
+    echo "Cleaning version in directory $DIR"
+    #ls -1t $DIR/content-version*gz
+    ls -1t $DIR/content-version*gz | head -n "-$ARC_VERSIONS" | xargs rm -f
 }
 
 DIRS=`find "$SEARCH_DIR" -type d`
 for DIR in $DIRS; do
-	cleanup_dir $DIR
+    cleanup_dir $DIR
 done
