0cdb99e80914b67c9e206ac1ec3baec21ffa145b
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2014-01-16 18:18:16 +0400

    Hide selectors from dependsOn API

--- site/engine_public/js/xsm.js
+++ site/engine_public/js/xsm.js
@@ -1,13 +1,12 @@
 function xsm_filter_form_autosubmit()
 {
     var form = $('#filter-form');
-    var controls = $(form).find('input');
+    var controls = form.find('input');
     for (var i in controls)
     {
         var control = controls[i];
         $(control).change(function() {
-            var form = $('#filter-form');
-            form.submit();
+            $('#filter-form').submit();
         });
     }
 }
@@ -15,14 +14,14 @@ function xsm_filter_form_autosubmit()
 
 function xsm_is_too_small_text(id)
 {
-    var val = $(id).val();
+    var val = $('#' + id).val();
     val = $.trim(val);
     return (val.length < 2);
 }
 
 function xsm_toggle_element(id, enabled)
 {
-    var node = $(id);
+    var node = $('#' + id);
     if (enabled)
     {
         node.removeAttr('disabled')
@@ -37,7 +36,7 @@ function xsm_toggle_element(id, enabled)
 
 function xsm_check_dependencies(id)
 {
-    var deps = $(id).data('dependencies');
+    var deps = $('#' + id).data('dependencies');
     if (!deps)
     {
         alert('Checking node with no dependencies. ');
@@ -55,14 +54,14 @@ function xsm_check_dependencies(id)
 
 function xsm_set_depends_on(id, dependency_id)
 {
-    var node = $(id);
+    var node = $('#' + id);
     var deps = node.data('dependencies');
     if (!deps)
         deps = [];
 
     deps.push(dependency_id);
     node.data('dependencies', deps);
-    var dep_node = $(dependency_id);
+    var dep_node = $('#' + dependency_id);
     dep_node.change(xsm_check_dependencies.bind(null, id));
     dep_node.bind('input', xsm_check_dependencies.bind(null, id));
 
