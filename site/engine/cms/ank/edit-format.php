f596df9000d846c44e8adca7e4522335fb1c4877
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2014-03-04 21:25:32 +0400

    Fixed #707 for course and person status edit

--- site/engine/cms/ank/edit-format.php
+++ site/engine/cms/ank/edit-format.php
@@ -3,7 +3,8 @@
 function xsm_is_bottom_field($key)
 {
     if (substr($key, strlen($key) - 8) == "_created" ||
-        substr($key, strlen($key) - 9) == "_modified")
+        substr($key, strlen($key) - 9) == "_modified" ||
+        substr($key, strlen($key) - 8) == "_deleted")
         return true;
     return false;
 }
@@ -12,13 +13,16 @@ function xsm_bottom_fields($table_name, $object)
 {
     $obj_created = xcms_get_key_or($object, "${table_name}_created");
     $obj_modified = xcms_get_key_or($object, "${table_name}_modified");
-    ?><div class="xsm-bottom-fields">
-        <b>Дата создания</b>: <?php echo $obj_created;
+    if (xu_not_empty($obj_created))
+    {?>
+        <div class="xsm-bottom-fields">
+            <b>Дата создания</b>: <?php echo $obj_created;
         if (xu_not_empty($obj_modified))
         {?>.
             <b>Последняя модификация</b>: <?php echo $obj_created;
         }?>
-    </div><?php
+        </div><?php
+    }
 }
 
 ?>
