734b2ac00904278ab1a85789b1eeb21e41cf7d4c
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2014-03-04 10:56:43 +0400

    Fix #385 and #398

--- tests/prepare-test-host.sh
+++ tests/prepare-test-host.sh
@@ -5,13 +5,13 @@
 set -e
 
 if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
-    echo "This script creates new test environment on test.fizlesh.ru using preproduction recipe."
+    echo "This script creates new test environment on test.fizlesh.ru using 'rc' recipe."
     exit 1
 fi
 
 tech_ssh="ssh tech@fizlesh.ru"
 
-$tech_ssh "sudo publish preproduction"
+$tech_ssh "sudo publish rc"
 $tech_ssh "sudo publish testing"
 
 echo "Testing prepared successfully"
