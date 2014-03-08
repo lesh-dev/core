5bc4a03aa118a71518874fe40f5a90f95897b10c
Superuser (probably, robot) <root@fizlesh.ru>
2014-03-09 00:12:34 +0400

    fixed publishtools version calculation (works wrong if run not from script directory.

--- tools/publish/version.sh
+++ tools/publish/version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 my_dir=$(dirname "$(readlink -f $0)")
-git_branch=$(git symbolic-ref HEAD | sed -e s#refs/heads/##)
-git_version=$(git rev-list --all | wc -l)
+git_branch=$(cd $my_dir; git symbolic-ref HEAD | sed -e s#refs/heads/##)
+git_version=$(cd $my_dir; git rev-list --all | wc -l)
 version=$(cat "$my_dir/../../site/VERSION")
 echo $git_branch-$version rev. $git_version
