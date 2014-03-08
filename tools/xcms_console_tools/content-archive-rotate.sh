cfa19071464502ac5df1ee82595b3283d59b011b
Dmitry Veltishev <vdm-photo@ya.ru>
2014-03-08 17:00:09 +0400

    added verbose flag.

--- tools/xcms_console_tools/content-archive-rotate.sh
+++ tools/xcms_console_tools/content-archive-rotate.sh
@@ -3,7 +3,7 @@
 # cleans old page versions
 
 if [ -z "$1" ]; then
-    echo "Usage: $0 <directory-to-clean-versions> [-n|--nothing] [versions-to-keep]"
+    echo "Usage: $0 <directory-to-clean-versions> [-n|--nothing] [-v|--verbose] [versions-to-keep]"
     exit 1
 fi
 
@@ -18,26 +18,37 @@ if [ "$1" == "-n" ] || [ "$1" == "--nothing" ]; then
     shift
 fi
 
+if [ "$1" == "-v" ] || [ "$1" == "--verbose" ]; then
+    VERBOSE="1"
+    shift
+fi
+
 ARC_VERSIONS=5
 
 if [ -n "$1" ]; then
     ARC_VERSIONS="$1"
 fi
 
-echo "Cleaning versions in $SEARCH_DIR"
+if [ "$VERBOSE" == "1" ]; then
+    echo "Cleaning versions in $SEARCH_DIR"
+fi    
 
 cleanup_dir()
 {
     local DIR="$1"
 
     if ! [ -r "$DIR/content" ]; then
-        echo "Directory $DIR has no content file, skipping. " 1>&2
+        if [ "$VERBOSE" == "1" ]; then
+            echo "Directory $DIR has no content file, skipping. " 1>&2
+        fi
         return 0
     fi
     if ! ls $DIR/content-version*gz &>/dev/null; then
         return 0
     fi
-    echo "Cleaning version in directory $DIR"
+    if [ "$VERBOSE" == "1" ]; then
+        echo "Cleaning version in directory $DIR"
+    fi
     #ls -1t $DIR/content-version*gz
     TO_REMOVE="`ls -1rt $DIR/content-version*gz | head -n "-$ARC_VERSIONS"`"
     if [ -n "$TO_REMOVE" ]; then
