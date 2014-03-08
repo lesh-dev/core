488b4864ae7989a0def1d31a6a805d1e07b8c577
Dmitry Veltishev <vdm-photo@ya.ru>
2014-03-08 13:28:10 +0400

    bug fixed in content archive rotation (wrong order of versions to remove); --nothing option added

--- tools/xcms_console_tools/content-archive-rotate.sh
+++ tools/xcms_console_tools/content-archive-rotate.sh
@@ -3,13 +3,20 @@
 # cleans old page versions
 
 if [ -z "$1" ]; then
-    echo "Usage: $0 <directory-to-clean-versions> [versions-to-keep]"
+    echo "Usage: $0 <directory-to-clean-versions> [-n|--nothing] [versions-to-keep]"
     exit 1
 fi
 
 SEARCH_DIR="`cd $1; pwd`"
 
 shift
+
+XARGS_COMMAND="rm -f"
+
+if [ "$1" == "-n" ] || [ "$1" == "--nothing" ]; then
+    XARGS_COMMAND="ls -l"
+    shift
+fi
 
 ARC_VERSIONS=5
 
@@ -22,7 +29,7 @@ echo "Cleaning versions in $SEARCH_DIR"
 cleanup_dir()
 {
     if ! [ -r "$DIR/content" ]; then
-        echo "Directory $DIR has no content, skipping. "
+        echo "Directory $DIR has no content file, skipping. "
         return 0
     fi
     if ! ls $DIR/content-version*gz &>/dev/null; then
@@ -30,7 +37,7 @@ cleanup_dir()
     fi
     echo "Cleaning version in directory $DIR"
     #ls -1t $DIR/content-version*gz
-    ls -1t $DIR/content-version*gz | head -n "-$ARC_VERSIONS" | xargs rm -f
+    ls -1rt $DIR/content-version*gz | head -n "-$ARC_VERSIONS" | xargs $XARGS_COMMAND
 }
 
 DIRS=`find "$SEARCH_DIR" -type d`
