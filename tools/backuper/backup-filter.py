481423f6bd120b62d8c9c6c20c9fcbdda6431da0
Dmitry Veltishev <vdm-photo@ya.ru>
2014-04-04 01:34:52 +0400

    pep8 issue 2

--- tools/backuper/backup-filter.py
+++ tools/backuper/backup-filter.py
@@ -11,16 +11,19 @@ import shutil
 
 ExpPowerBase = 1.2
 
+
 def isBackupDir(x):
     name = os.path.basename(x)
     m = re.match("\d\d\d\d-\d\d-\d\d", name)
     return True if m else False
-    
+
+
 def getBackupList(baseDir):
     dirList = filter(os.path.isdir, [os.path.join(baseDir, x) for x in os.listdir(baseDir)])
     backupList = sorted(filter(isBackupDir, dirList))
     backupList.reverse()
     return backupList
+
 
 def parseDate(name):
     try:
@@ -29,17 +32,18 @@ def parseDate(name):
             raise RuntimeError("Cannot parse date format for '" + name + "'. ")
         if m.groups < 3:
             raise RuntimeError("Cannot parse date format for '" + name + "' (matches count less than 3). ")
-        
-        (year, month, day) = (int(x) for x in m.group(1,2,3))
+
+        (year, month, day) = (int(x) for x in m.group(1, 2, 3))
         return datetime.datetime(year, month, day)
-    
+
     except (ValueError, RuntimeError):
         sys.stderr.write("Warning: cannot parse backup date '" + name + "'. Skipping this directory.\n")
         return None
-        
-    
+
+
 def extractDate(x):
     return parseDate(os.path.basename(x))
+
 
 def getRemoveList(backupList):
     if not backupList:
@@ -49,18 +53,18 @@ def getRemoveList(backupList):
     firstBackupDate = extractDate(backupList[0])
     if not firstBackupDate:
         raise RuntimeError("Latest backup directory has incorrect format. ")
-    
+
     remainDates = set([firstBackupDate])
-    
+
     # make exponential backup row - from latest
     days = 0
     power = 0
     while days < len(backupList):
         days = int(ExpPowerBase ** power)
         power += 1
-        leaveDate = firstBackupDate - datetime.timedelta(days = days)
+        leaveDate = firstBackupDate - datetime.timedelta(days=days)
         remainDates.add(leaveDate)
-    
+
     removeList = []
     for x in backupList:
         date = extractDate(x)
@@ -68,11 +72,10 @@ def getRemoveList(backupList):
             removeList.append(x)
 
     return removeList
-   
-# -------------------------------
 
+# -------------------------------
 cmdArgs = sys.argv[1:]
-    
+
 if not cmdArgs:
     print "Sorry, no parameters. Syntax: backup-filter.py [-l|--list-only] <directory-to-clean>"
     sys.exit(2)
@@ -86,7 +89,7 @@ if cmdArgs[0] == "-l" or cmdArgs[0] == "--list-only":
 if not cmdArgs:
     print "Sorry, 'directory-to-clean' not specified. Syntax: backup-filter.py [-l|--list-only] <directory-to-clean>"
     sys.exit(2)
-    
+
 baseDir = cmdArgs.pop(0)
 if cmdArgs:
     print "Error: trailing command-line parameters detected. Switches should preceed <path-to-clean>."
@@ -103,4 +106,3 @@ if doListOnly:
 else:
     for x in toRemove:
         shutil.rmtree(x)
-        
