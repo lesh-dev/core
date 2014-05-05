c78f3ecacc4b1870c17ebfa5124875a74f1102d8
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2014-05-05 11:09:43 +0400

    Improved prepare test host

--- tests/prepare-test-host.sh
+++ tests/prepare-test-host.sh
@@ -4,15 +4,31 @@
 
 set -e
 
-if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+print_usage()
+{
     cat <<EOF
 This script creates new test environment on test.fizlesh.ru using 'rc' recipe.
 Usage: $0 [options]
 Options are:
     -c|--clear-db        Perform database cleanup on testing instance
+    -h|--help            This help
 EOF
     exit 1
-fi
+}
+
+
+while [ -n "$1" ] ; do
+    if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+        print_usage
+    elif [ "$1" = "-c" ] || [ "$1" = "--clear-db" ] ; then
+        init_test_db=yes
+        shift || true
+        continue
+    else
+        echo "Unknown option '$1'"
+        print_usage
+    fi
+done
 
 site="--site fizlesh.ru"
 tech_ssh="ssh tech@fizlesh.ru"
@@ -20,7 +36,7 @@ tech_ssh="ssh tech@fizlesh.ru"
 $tech_ssh "sudo publish $site rc"
 $tech_ssh "sudo publish $site testing"
 
-if [ "$1" = "-c" ] || [ "$1" = "--clear-db" ] ; then
+if [ -n "$init_test_db" ] ; then
     $tech_ssh "sudo publish $site init-test-db"
 fi
 
