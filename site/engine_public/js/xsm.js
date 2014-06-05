a5069825649d0d03c19d61226fd5baba70e5205a
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2014-06-05 23:33:52 +0400

    New status should be different from old one.
    To make this possible, a kind of custom dependsOn handlers
    was implemented. Custom handler can return bool value that
    specifies that field value is good enough for submission.

--- site/engine_public/js/xsm.js
+++ site/engine_public/js/xsm.js
@@ -11,12 +11,20 @@ function xsm_filter_form_autosubmit()
     }
 }
 
-
 function xsm_is_too_small_text(id)
 {
     var val = $('#' + id).val();
     val = $.trim(val);
-    return (val.length < 2);
+    return (val.length < 10);
+}
+
+function xsm_check_custom_dep_handler(id)
+{
+    var control = $('#' + id);
+    var handler = control.data('custom-dep-handler');
+    if (!handler)
+        return null;
+    return handler(id);
 }
 
 function xsm_toggle_element(id, enabled)
@@ -46,13 +54,20 @@ function xsm_check_dependencies(id)
     var enabled = true;
     for (var i in deps)
     {
+        var custom_result = xsm_check_custom_dep_handler(deps[i])
+        if (custom_result !== null)
+        {
+            enabled = enabled && custom_result;
+            continue;
+        }
+
         if (xsm_is_too_small_text(deps[i]))
             enabled = false;
     }
     xsm_toggle_element(id, enabled);
 }
 
-function xsm_set_depends_on(id, dependency_id)
+function xsm_set_depends_on(id, dependency_id, custom_handler)
 {
     var node = $('#' + id);
     var deps = node.data('dependencies');
@@ -62,6 +77,9 @@ function xsm_set_depends_on(id, dependency_id)
     deps.push(dependency_id);
     node.data('dependencies', deps);
     var dep_node = $('#' + dependency_id);
+    if (custom_handler)
+        dep_node.data('custom-dep-handler', custom_handler);
+
     dep_node.change(xsm_check_dependencies.bind(null, id));
     dep_node.bind('input', xsm_check_dependencies.bind(null, id));
 
