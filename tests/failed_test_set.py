2fcbf92f4693862ca2cce2615c1f81990b2d077a
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2014-07-17 11:52:31 +0400

    Tabs are bad thing, even in help

--- tests/failed_test_set.py
+++ tests/failed_test_set.py
@@ -3,8 +3,10 @@
 #
 # This file is auto-generated
 
+import xcms_auth_check_dup_login
 
 
 def getTests(baseUrl, args):
     return {
+        "xcms_auth_check_dup_login.py": xcms_auth_check_dup_login.XcmsAuthCheckDupLogin(baseUrl, args),
     }
