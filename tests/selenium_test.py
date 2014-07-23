ca3491ec8d0c1442339904ca155b688befa2d41c
Dmitry Veltishev <vdm-photo@ya.ru>
2014-07-24 00:47:44 +0400

    suspicuous word list enhanced. (after #795)

--- tests/selenium_test.py
+++ tests/selenium_test.py
@@ -828,7 +828,7 @@ class SeleniumTest(object):
             returns True and suspicious word if error found, False/None otherwise
         """
         pageText = self.getPageSource()
-        susp = ["Notice:", "Error:", "Warning:", "Fatal error:", "Parse error:"]
+        susp = ["Notice:", "Error:", "Warning:", "Fatal error:", "Parse error:", "Can&apos;t find element:"]
         for word in susp:
             if (word in pageText) and (" on line " in pageText):
                 self.logAdd("PHP ERROR " + userSerialize(word) + " detected on page " + userSerialize(self.curUrl()) + ": ")
