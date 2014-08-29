b17b872f8b4958bbf630a343f758de3acd0903d1
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2014-08-29 23:08:52 +0400

    Fix code style in resample

--- site/engine/sys/resample.php
+++ site/engine/sys/resample.php
@@ -1,27 +1,29 @@
 <?php
-    $rezX=0;
-    $rezY=0;
+    $rezX = 0;
+    $rezY = 0;
 
     /**
       * @param sizeType is 'X' (to make resize based on X dimension
       * with preserving aspect ratio) or any other value
       **/
-    function resizeJPEG($fileFrom, $fileTo, $newSize, $sizeType, $JPEGQuality=100)
+    function resizeJPEG($fileFrom, $fileTo, $newSize, $sizeType, $JPEGQuality = 100)
     {
         global $rezX;
         global $rezY;
+
         $source = imagecreatefromjpeg($fileFrom);
-        if($source=="")
+        if (xu_empty($source))
         {
-            echo "<b>Can't open image. '$fileFrom'</b>";
+            echo "<b>Can't open image '$fileFrom'</b>";
             return;
         }
         $srcX = imagesx($source);
         $srcY = imagesy($source);
-        if ($sizeType=='X') $ratio = $newSize/$srcX;
-        else  $ratio = $newSize/$srcY;
-        $destX = $srcX*$ratio;
-        $destY = $srcY*$ratio;
+        if ($sizeType == 'X')
+            $ratio = $newSize / $srcX;
+        else  $ratio = $newSize / $srcY;
+        $destX = $srcX * $ratio;
+        $destY = $srcY * $ratio;
         $destImage = imagecreateTrueColor($destX, $destY);
 
         imagecopyresampled($destImage, $source, 0, 0, 0, 0, $destX, $destY, $srcX, $srcY);
@@ -30,17 +32,18 @@
         imagedestroy($destImage);
         $rezX = (int)$destX;
         $rezY = (int)$destY;
-        if(file_exists($fileTo)) return true;
+        if (file_exists($fileTo))
+            return true;
         return false;
     }
 
-    function make640($fileFrom, $fileTo, $JPEGQuality=75)
+    function make640($fileFrom, $fileTo, $JPEGQuality = 75)
     {
         $source = imagecreatefromjpeg($fileFrom);
         $srcX = imagesx($source);
         $srcY = imagesy($source);
         imagedestroy($source);
-        if ($srcX>$srcY)
+        if ($srcX > $srcY)
         {
             $type = 'Y';
             $dest = 480;
@@ -53,7 +56,7 @@
         return resizeJPEG($fileFrom, $fileTo, $dest, $type, $JPEGQuality);
     }
 
-    function preview($fileFrom, $fileTo, $size=100, $JPEGQuality=75)
+    function preview($fileFrom, $fileTo, $size = 100, $JPEGQuality = 75)
     {
         return resizeJPEG($fileFrom, $fileTo, $size, 'Y', $JPEGQuality);
     }
