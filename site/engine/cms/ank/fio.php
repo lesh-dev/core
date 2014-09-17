00909fb49d60cccff56d421683f42f567d108e88
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2014-09-17 21:59:02 +0400

    Fix #821 (PHP error in user profile viewer from admin page)

---
+++ site/engine/cms/ank/fio.php
@@ -0,0 +1,35 @@
+<?php
+/**
+  * Canonical way to obtain FIO
+  **/
+function xsm_fio($person)
+{
+    return
+        xcms_get_key_or($person, 'last_name').' '.
+        xcms_get_key_or($person, 'first_name').' '.
+        xcms_get_key_or($person, 'patronymic');
+}
+/**
+  * HTML-encoded version of @c xsm_fio
+  **/
+function xsm_fio_enc($person)
+{
+    return htmlspecialchars(xsm_fio($person));
+}
+
+/**
+  * Canonical way to obtain FI
+  **/
+function xsm_fi($person, $prefix = '')
+{
+    return $person["${prefix}last_name"].' '.$person["${prefix}first_name"];
+}
+/**
+  * HTML-encoded version of @c xsm_fi
+  **/
+function xsm_fi_enc($person, $prefix = '')
+{
+    return htmlspecialchars(xsm_fi($person, $prefix));
+}
+
+?>
