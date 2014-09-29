b7480f748ba2da2db8b2ff8fe6e1c1e00dc23a9e
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2014-09-30 00:46:18 +0400

    XSS fixed

--- site/engine/contest/forms.php
+++ site/engine/contest/forms.php
@@ -2,48 +2,60 @@
 
 function xcmst_draw_contest_form_field($type, $name, $value, $id)
 {
+    $value = htmlspecialchars($value);
     if ($type == "pk")
     {
         if (!strlen($value))
             $value = XDB_NEW;
-        echo "<input type='hidden' name='$id' value='$value' />";
+        echo "<input type=\"hidden\" name=\"$id\" value=\"$value\" />";
     }
     if ($type == "text")
     {
-        echo "<tr><td class='font-wieght: bold;'>$name</td>
-            <td><input type='text' placeholder='$name' name='$id' value='{$value}'/></td></tr>\n";
+        echo "<tr>\n".
+            "<td>$name</td>\n".
+            "<td><input type=\"text\" placeholder=\"$name\" name=\"$id\" id=\"$id-input\" value=\"$value\" /></td>\n".
+            "</tr>\n";
     }
     if ($type == "file")
     {
-        echo "<tr><td>$name</td>
-            <td><input name='$id' type=\"file\" value=\"$value\"/></td></tr>\n";
+        echo "<tr>\n".
+            "<td>$name</td>\n".
+            "<td><input name=\"$id\" id=\"$id-file\" type=\"file\" value=\"$value\" /></td>\n".
+            "</tr>\n";
     }
     if ($type == "large")
     {
-        echo "<tr><td colspan='2'>$name:</td>
-            <tr><td colspan='2'>
-                <textarea class='field span12' rows='10' name='$id'>$value</textarea></td></tr>\n";
+        echo "<tr><td colspan=\"2\">$name:</td></tr>\n".
+            "<tr><td colspan=\"2\">\n".
+            "<textarea class=\"field span12\" rows=\"8\" name=\"$id\" id=\"$id-text\">$value</textarea></td>\n".
+            "</tr>\n";
     }
 }
 
 function xcmst_draw_contest_form_view($type, $name, $value)
 {
     global $web_prefix;
+    $value = htmlspecialchars($value);
     if ($type == "pk")
     {
 
     }
-    if ($type == "file")
+    elseif ($type == "file")
     {
-        echo "<tr><th>$name</th><td><a class='btn btn-success' href='/$web_prefix$value'>Скачать</a></td></tr>\n";
+        echo "<tr><th>$name</th><td>";
+        if (xu_not_empty($value))
+            echo "<a class=\"btn btn-success\" href=\"/$web_prefix$value\">Скачать</a>";
+        else
+            echo "(нет файла)";
+        echo "</td></tr>\n";
     }
-    if ($type == "text")
+    elseif ($type == "text")
     {
         echo "<tr><th>$name</th><td>$value</td></tr>\n";
     }
-    if ($type == "large")
+    elseif ($type == "large")
     {
-        echo "<tr><th colspan=2>$name:</th></tr>";
-        echo "<tr><td colspan=2>$value</td></tr>\n";
+        echo "<tr><th colspan=\"2\">$name:</th></tr>\n";
+        echo "<tr><td colspan=\"2\">$value</td></tr>\n";
     }
 }
