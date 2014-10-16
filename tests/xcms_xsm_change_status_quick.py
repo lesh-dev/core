27b130b9dfb6d41d297ff62c42d47557175ed3a5
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2014-10-16 11:07:02 +0400

    Please obey PEP8

--- tests/xcms_xsm_change_status_quick.py
+++ tests/xcms_xsm_change_status_quick.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 
-import xtest_common, random_crap
+import xtest_common
+import random_crap
+
 
 class XcmsXsmChangeStatusQuick(xtest_common.XcmsTest):
     """
@@ -19,41 +21,39 @@ class XcmsXsmChangeStatusQuick(xtest_common.XcmsTest):
         testMailPrefix = self.m_conf.getAnketaNamePrefix()
 
         self.performLoginAsManager()
-        
+
         self.gotoXsm()
         self.gotoXsmActive()
         self.gotoXsmAddPerson()
-        
+
         # generate
-        inpLastName = testMailPrefix + u"Статусов" + random_crap.randomText(4);
+        inpLastName = testMailPrefix + u"Статусов" + random_crap.randomText(4)
         inpFirstName = u"Иннокентий_" + random_crap.randomText(3)
         inpMidName = u"Петрович_" + random_crap.randomText(3)
 
         inpLastName = self.fillElementById("last_name-input", inpLastName)
         inpFirstName = self.fillElementById("first_name-input", inpFirstName)
         inpMidName = self.fillElementById("patronymic-input", inpMidName)
-        
+
         self.clickElementById("update-person-submit")
-        
+
         self.gotoBackToPersonView()
 
         fullAlias = inpLastName + " " + inpFirstName + " " + inpMidName
         # check if person alias is present (person saved correctly)
-        
+
         self.checkPersonAliasInPersonView(fullAlias)
-        
+
         self.assertElementTextById("anketa_status-span", u"Активный")
-     
+
         self.gotoXsmChangePersonStatus()
-        
+
         self.setOptionValueByIdAndValue("anketa_status-selector", "discuss")
         commentText = u"Комментарий к смене статуса " + random_crap.randomCrap(5)
         commentText = self.fillElementById("comment_text-text", commentText)
-                
+
         self.clickElementById("update-person_comment-submit-top")
         self.gotoBackToPersonView()
-        
+
         self.assertBodyTextPresent(u"Статус Активный изменён на Обсуждается")
         self.assertBodyTextPresent(commentText)
-
-
