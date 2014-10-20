b182fcd17f9d4079d4e104df656af68aae777cfb
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2014-10-21 00:06:57 +0400

    More code style in tests

--- tests/xcms_metrics_check.py
+++ tests/xcms_metrics_check.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 
-import xtest_common, random_crap
+import xtest_common
+
 
 class XcmsMetricsCheck(xtest_common.XcmsBaseTest):
-	"""
-	This test checks if metrics counter successfully wiped off from test website.
-	"""
-	def run(self):
+    """
+    This test checks if metrics counter successfully wiped off from test website.
+    """
+    def run(self):
 
-		self.setAutoPhpErrorChecking(True)
-		
-		self.assertNoInstallerPage()
-		
-		self.gotoRoot()
-		self.assertSourceTextNotPresent("Metrika")
-		
-    
+        self.setAutoPhpErrorChecking(True)
+
+        self.assertNoInstallerPage()
+
+        self.gotoRoot()
+        self.assertSourceTextNotPresent("Metrika")
--- tests/xcms_xsm_phones.py
+++ tests/xcms_xsm_phones.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 
-import xtest_common, random_crap
+import xtest_common
+import random_crap
+
 
 class XcmsXsmPhones(xtest_common.XcmsTest):
     """
@@ -15,16 +17,16 @@ class XcmsXsmPhones(xtest_common.XcmsTest):
 
     def phoneFix(self, phone):
         return phone.replace("+7", "8").replace("8-900-", "8(900)")
-    
+
     def run(self):
         self.performLoginAsManager()
 
         self.gotoXsm()
         self.gotoXsmActive()
         self.gotoXsmAddPerson()
-        
+
         # generate
-        inpLastName = u"Телефонов_" + random_crap.randomText(4);
+        inpLastName = u"Телефонов_" + random_crap.randomText(4)
         inpFirstName = u"Самсунг_" + random_crap.randomText(3)
         inpMidName = u"Нокиевич_" + random_crap.randomText(3)
         inpCellPhones = ["+7(900)000-00-00", "+7(900)999-99-99"]
@@ -37,35 +39,35 @@ class XcmsXsmPhones(xtest_common.XcmsTest):
         inpMidName = self.fillElementById("patronymic-input", inpMidName)
         inpCellPhoneLine = self.fillElementById("cellular-input", inpCellPhoneLine)
         inpPhoneLine = self.fillElementById("phone-input", inpPhoneLine)
-        
+
         inpCellPhones = list(map(self.phoneFix, inpCellPhones))
         inpPhones = list(map(self.phoneFix, inpPhones))
-        
+
         self.clickElementById("update-person-submit")
-        
+
         self.gotoBackToPersonView()
 
         fullAlias = xtest_common.fullAlias(inpLastName, inpFirstName, inpMidName)
         # check if person alias is present (person saved correctly)
-        
+
         self.checkPersonAliasInPersonView(fullAlias)
-        
+
         personId = self.getCurrentPersonId()
-        
-        for i in range(0,2):
+
+        for i in range(0, 2):
             personCellPhoneEleId = "person{0}-cellular-{1}".format(personId, i)
             personPhoneEleId = "person{0}-phone-{1}".format(personId, i)
-            
+
             siteCellPhone = self.getElementTextById(personCellPhoneEleId)
             sitePhone = self.getElementTextById(personPhoneEleId)
             self.logAdd("Cell phone #{0} on the site: ".format(i) + siteCellPhone)
             self.logAdd("Phone #{0} on the site: ".format(i) + sitePhone)
-        
+
             if inpCellPhones[i] != siteCellPhone:
                 self.failTest("Cell phone #{0} on the site '{1}' don't match entered '{2}'. ".format(i, siteCellPhone, inpCellPhones[i]))
             if inpPhones[i] != sitePhone:
                 self.failTest("Phone #{0} on the site '{1}' don't match entered '{2}'. ".format(i, sitePhone, inpPhones[i]))
-                
+
         self.gotoEditPerson()
         inpPhone = "+79261112233"
         inpPhone = self.fillElementById("phone-input", inpPhone)
@@ -73,13 +75,13 @@ class XcmsXsmPhones(xtest_common.XcmsTest):
         inpCellPhone = self.fillElementById("cellular-input", inpCellPhone)
         self.clickElementById("update-person-submit")
         self.gotoBackToPersonView()
-        
+
         personCellPhoneEleId = "person{0}-cellular-{1}".format(personId, 0)
         personPhoneEleId = "person{0}-phone-{1}".format(personId, 0)
-        
+
         siteCellPhone = self.getElementTextById(personCellPhoneEleId)
         sitePhone = self.getElementTextById(personPhoneEleId)
-        
+
         if sitePhone != "8(926)111-22-33":
             self.failTest("Phone was not autoformatted. ")
         if siteCellPhone != "8(926)111-22-33":
