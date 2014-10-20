4a6de059154b6c9bf136189e9577d5ea2fe55c00
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2014-10-20 22:00:36 +0400

    A bit of PEP8

--- tests/xcms_auth_set_dup_email_by_admin.py
+++ tests/xcms_auth_set_dup_email_by_admin.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 
-import xtest_common, random_crap
+import xtest_common
+import random_crap
+
 
 class XcmsAuthSetDuplicateEmailByAdmin(xtest_common.XcmsTest):
     """
@@ -11,65 +13,63 @@ class XcmsAuthSetDuplicateEmailByAdmin(xtest_common.XcmsTest):
     create some user A
     create another user B
     set user B's email to email of user A.
-    
+
     """
-    
+
     def run(self):
-       
+
         # step one: create first user
         inpLogin1 = "dup_mail1_" + random_crap.randomText(8)
         inpEMail1 = random_crap.randomEmail()
         inpPass1 = random_crap.randomText(10)
         inpName1 = u"Вася Тестов" + random_crap.randomText(6)
-        
+
         inpLogin1, inpEMail1, inpPass1, inpName1 = self.createNewUser(inpLogin1, inpEMail1, inpPass1, inpName1, ["do_not_logout_admin"])
-        
+
         # step 2: create another user
         inpLogin2 = "dup_mail2_" + random_crap.randomText(8)
         inpEMail2 = random_crap.randomEmail()
         inpPass2 = random_crap.randomText(10)
         inpName2 = u"Миша Тестов" + random_crap.randomText(6)
-        
+
         # create second user without re-login Admin
-        
+
         inpLogin2, inpEMail2, inpPass2, inpName2 = self.createNewUser(inpLogin2, inpEMail2, inpPass2, inpName2, ["do_not_login_as_admin"])
-        
+
         print "logging as created first user. "
         if not self.performLogin(inpLogin1, inpPass1):
             self.failTest("Cannot login as newly created user One. ")
-        
+
         self.performLogoutFromSite()
-        
+
         print "logging as created second user. "
         if not self.performLogin(inpLogin2, inpPass2):
             self.failTest("Cannot login as newly created user Two. ")
-        
+
         self.performLogoutFromSite()
-        
+
         # login as admin, enter user profile and change some fields.
-        
+
         self.performLoginAsAdmin()
-        
+
         self.gotoAdminPanel()
         self.gotoUserList()
-        
+
         print "enter user profile in admin CP"
-        
+
         self.gotoUrlByPartialLinkText(inpLogin2)
-        
+
         self.assertElementValueById("name-input", inpName2)
         self.assertElementValueById("email-input", inpEMail2)
-        
+
         # try to hack email
         inpEMail2new = inpEMail1
-        
+
         inpEMail2new = self.fillElementById("email-input", inpEMail2new)
         print "new email 2: ", inpEMail2new
-        
+
         self.clickElementById("update_user")
-        
+
         self.assertBodyTextPresent(u"уже существует")
-        
+
         #self.performLogoutFromAdminPanel()
-        
-        
--- tests/xcms_content_bad_path_page.py
+++ tests/xcms_content_bad_path_page.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 
-import xtest_common, random_crap
+import xtest_common
+import random_crap
+
 
 class XcmsContentBadPathPage(xtest_common.XcmsTest):
     """
@@ -22,19 +24,18 @@ class XcmsContentBadPathPage(xtest_common.XcmsTest):
         self.gotoUrlByLinkText(self.m_parentPage)
         self.gotoCreatePage()
 
-        inpPageDir = "../../hacked_page_" + random_crap.randomText(8);
-        inpMenuTitle = "hacked_menu_title_" + random_crap.randomText(8);
-        inpPageHeader = "hacked_header_" + random_crap.randomText(8);
-        inpAlias = "hacked/page/alias/" + random_crap.randomText(8);
+        inpPageDir = "../../hacked_page_" + random_crap.randomText(8)
+        inpMenuTitle = "hacked_menu_title_" + random_crap.randomText(8)
+        inpPageHeader = "hacked_header_" + random_crap.randomText(8)
+        inpAlias = "hacked/page/alias/" + random_crap.randomText(8)
 
-        inpPageDir = self.fillElementById("create-name-input", inpPageDir);
-        inpMenuTitle = self.fillElementById("menu-title-input", inpMenuTitle);
-        inpPageHeader = self.fillElementById("header-input", inpPageHeader);
-        inpAlias = self.fillElementById("alias-input", inpAlias);
+        inpPageDir = self.fillElementById("create-name-input", inpPageDir)
+        inpMenuTitle = self.fillElementById("menu-title-input", inpMenuTitle)
+        inpPageHeader = self.fillElementById("header-input", inpPageHeader)
+        inpAlias = self.fillElementById("alias-input", inpAlias)
 
         self.clickElementById("create-page-submit")
         self.assertBodyTextPresent(u"Недопустимый физический путь страницы")
 
-        # ensure that page was not actually created. 
+        # ensure that page was not actually created.
         self.assertUrlNotPresent(inpMenuTitle)
-
