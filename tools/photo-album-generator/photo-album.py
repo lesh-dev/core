a529685bca2ef6c4e75bc689389ff03d061c310a
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2014-10-21 09:53:59 +0400

    Fix code style

--- tools/photo-album-generator/photo-album.py
+++ tools/photo-album-generator/photo-album.py
@@ -5,35 +5,37 @@ num = 0
 print open("header.html").read()
 
 for fileName in os.listdir("."):
-#	print fileName
-	if ".jpg" not in fileName:
-#		print "not jpeg"
-		continue
-	descFile = fileName.replace(".jpg", ".txt")
-	desc = ""
-	try:
-		df = open(descFile)
-		desc = df.read().strip()
-	except IOError:
-		pass
-	imgClass = "album-left"
-	if num == 1:
-		imgClass = "album-right"
-		
-	print '<img class="' + imgClass + '" src="/flpic/' + fileName + '" alt="' + fileName + '"></img>'
-	print '<p class="' + imgClass + '">'
-	if num == 0:
-		print '<img class="album-arrow-left" src="/flpic/misc/arrow-left-small.png" alt="arrow left" title="' + fileName + '"></img>'	
-	if num == 1:
-		print '<img class="album-arrow-right" src="/flpic/misc/arrow-right-small.png" alt="arrow right" title="' + fileName + '"></img>'	
+    # print fileName
 
-	print desc 
-	print '</p>'
-	print ''
-	if True or num == 1:
-		print '<div style="clear:both;"></div>'
-		print ''
+    if ".jpg" not in fileName:
+        # print "not jpeg"
+        continue
 
-	num = (num + 1) % 2
+    descFile = fileName.replace(".jpg", ".txt")
+    desc = ""
+    try:
+        df = open(descFile)
+        desc = df.read().strip()
+    except IOError:
+        pass
+    imgClass = "album-left"
+    if num == 1:
+        imgClass = "album-right"
+
+    print '<img class="' + imgClass + '" src="/flpic/' + fileName + '" alt="' + fileName + '"></img>'
+    print '<p class="' + imgClass + '">'
+    if num == 0:
+        print '<img class="album-arrow-left" src="/flpic/misc/arrow-left-small.png" alt="arrow left" title="' + fileName + '"></img>'
+    if num == 1:
+        print '<img class="album-arrow-right" src="/flpic/misc/arrow-right-small.png" alt="arrow right" title="' + fileName + '"></img>'
+
+    print desc
+    print '</p>'
+    print
+    if True or num == 1:
+        print '<div style="clear:both;"></div>'
+        print ''
+
+    num = (num + 1) % 2
 
 print "</div>"
