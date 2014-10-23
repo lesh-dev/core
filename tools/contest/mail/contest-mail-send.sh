edcc1a7f0ccce57afad5dc353661599237ce5ba7
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2014-10-23 09:07:04 +0400

    Fix typos in enhanced script

--- tools/contest/mail/contest-mail-send.sh
+++ tools/contest/mail/contest-mail-send.sh
@@ -5,8 +5,8 @@ mail_list="list"
 echo "Sending contest from $mail_list"
 for addr in $(cat $mail_list); do
     mutt \
-        -F muttrc
-        -a pravila-olimpiada-fizlesh-contest-2014-2015.pdf
+        -F muttrc \
+        -a pravila-olimpiada-fizlesh-contest-2014-2015.pdf \
         -a usloviya-olimpiada-fizlesh-contest-2014-2015.pdf \
         -s 'О Межрегиональной Физической Олимпиаде 2014-2015' \
         -- $addr < letter2014.txt
