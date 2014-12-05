685a086efaa3e629cc61cc275f3b016a4824feb6
Dmitry Veltishev <vdm-photo@ya.ru>
2014-12-06 02:39:07 +0300

    anketa duplicate stress test

---
+++ tests/xcms_xsm_anketa_dup_stress.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+import xtest_common, random_crap
+
+class XcmsXsmAnketaDupStress(xtest_common.XcmsTest):
+    """
+    This test checks duplicate anketa merging algorithm.
+    """
+    
+    def generateData(self):
+        self.inpLastName = u"Спамеров" + random_crap.randomText(4)
+        self.inpFirstName = u"Егор" + random_crap.randomText(3)
+        self.inpMidName = u"Федорович" + random_crap.randomText(2)
+        
+        self.inpBirthDate = random_crap.randomDigits(2) + "." + random_crap.randomDigits(2) + "." + random_crap.randomDigits(4);
+        
+        self.inpSchool = u"Школа спамеров №" + random_crap.randomDigits(4)
+        
+        self.inpSchoolCity = u"Спамерово-" + random_crap.randomDigits(2)
+        self.inpClass = random_crap.randomDigits(1) + u" Жэ"
+        
+        self.inpPhone = "+7" + random_crap.randomDigits(9)
+        self.inpCell = "+7" + random_crap.randomDigits(9)
+        self.inpEmail = random_crap.randomText(7) + "@" + random_crap.randomText(5) + ".ru"
+        self.inpSkype = random_crap.randomText(8)
+        self.inpSocial = random_crap.randomVkontakte()
+        
+        self.inpFav = random_crap.randomCrap(20, ["multiline"])
+        self.inpAch = random_crap.randomCrap(13, ["multiline"])
+        self.inpHob = random_crap.randomCrap(24, ["multiline"])
+        self.inpSource = random_crap.randomCrap(20, ["multiline"])
+
+    def addAnketa(self):
+        self.gotoRoot()
+        
+        #navigate to anketas
+        
+        self.gotoUrlByLinkText(self.getEntranceLinkName())
+        self.gotoAnketa()
+        self.assertBodyTextPresent(self.getAnketaPageHeader())
+            
+        # generate
+        self.inpLastNameReal = self.fillElementById("last_name-input", self.inpLastName)
+        self.inpFirstNameReal = self.fillElementById("first_name-input", self.inpFirstName)
+        self.inpMidNameReal = self.fillElementById("patronymic-input", self.inpMidName)
+        self.inpBirthDateReal = self.fillElementById("birth_date-input", self.inpBirthDate)
+        self.inpSchoolReal = self.fillElementById("school-input", self.inpSchool)
+        self.inpSchoolCityReal = self.fillElementById("school_city-input", self.inpSchoolCity)
+        self.inpClassReal = self.fillElementById("current_class-input", self.inpClass)
+        self.inpPhoneReal = self.fillElementById("phone-input", self.inpPhone)
+        self.inpEmailReal = self.fillElementById("email-input", self.inpEmail)
+        self.inpFavReal = self.fillElementById("favourites-text", self.inpFav)
+        self.inpAchReal = self.fillElementById("achievements-text", self.inpAch)
+        self.inpHobReal = self.fillElementById("hobby-text", self.inpHob)
+        self.inpSourceReal = self.fillElementById("lesh_ref-text", self.inpSource)
+        
+        self.clickElementById("submit-anketa-button")
+    
+    def checkUniqueAnketa(self):
+            
+        # now login as admin
+        self.personAlias = xtest_common.shortAlias(self.inpLastNameReal, self.inpFirstNameReal)
+        # self.personAlias = xtest_common.fullAlias(self.inpLastNameReal, self.inpFirstNameReal, self.inpMidNameReal)
+        
+        self.performLoginAsManager()
+        self.gotoRoot()
+        self.gotoUrlByLinkText(self.getAnketaListMenuName())
+        
+        alias = self.fillElementById("show_name_filter-input", self.personAlias)
+        self.clickElementByName("show-person")
+        if self.countIndexedUrlsByLinkText(self.personAlias) != 1:
+            self.failTest("Found more than one anketa with exact FIO. Duplicate filtering is broken. ")
+            
+    def changeStatus(self):
+        self.gotoXsm()
+        self.gotoXsmAnketas()
+        self.gotoUrlByLinkText(self.personAlias)
+        self.gotoXsmChangePersonStatus()
+        
+        self.setOptionValueByIdAndValue("anketa_status-selector", "nextyear")
+        commentText = u"Меняем статус первой анкете: " + random_crap.randomCrap(5)
+        commentText = self.fillElementById("comment_text-text", commentText)
+                
+        self.clickElementById("update-person_comment-submit-top")
+        self.gotoBackToPersonView()
+        
+        self.newState = u"Отложен"
+        self.assertBodyTextPresent(u"Статус Новый изменён на {0}".format(self.newState))
+        self.assertBodyTextPresent(commentText)
+    
+    def checkStatus(self):
+        self.gotoXsm()
+        self.gotoXsmAnketas()
+        self.gotoUrlByLinkText(self.personAlias)
+        # anketa should change status to new (like 'ticket reopen')
+        self.assertElementTextById("anketa_status-span", u"Новый")
+
+    # -------------------- begining of the test
+    def run(self):
+        # add anketa one
+        self.generateData()
+        self.addAnketa()
+        self.assertBodyTextPresent(self.getAnketaSuccessSubmitMessage())
+        self.addAnketa()
+        self.assertBodyTextNotPresent(self.getAnketaSuccessSubmitMessage())
+        self.assertBodyTextPresent(self.getAnketaDuplicateSubmitMessage())
+        
+        self.checkUniqueAnketa()
+        
+        self.changeStatus()
+        
+        self.addAnketa()
+        self.assertBodyTextNotPresent(self.getAnketaSuccessSubmitMessage())
+        self.assertBodyTextPresent(self.getAnketaDuplicateSubmitMessage())
+        
+        self.checkStatus()
