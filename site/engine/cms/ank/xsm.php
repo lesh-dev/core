884bc684a398fa0718e1605692b7ace96fe0d605
Mikhail Veltishchev <EMAIL>
2014-12-10 00:57:30 +0300

    Experimental add-person_school, does not work yet. Refactor url selection a bit.

---
+++ site/engine/cms/ank/xsm.php
@@ -0,0 +1,13 @@
+<?php
+
+function xsm_valid_aparam($aparam)
+{
+    return (
+        substr($aparam, 0, 5) == "list-" ||
+        substr($aparam, 0, 5) == "view-" ||
+        substr($aparam, 0, 5) == "edit-" ||
+        substr($aparam, 0, 4) == "add-" ||
+        false
+    );
+}
+?>
