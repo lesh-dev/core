7023b3db87278e225d835a9b4345566e57494650
Mikhail Veltishchev <mvel@yandex-team.ru>
2014-12-16 00:44:54 +0300

    Initial settings

--- site/engine_public/js/xsm_add_person_school.js
+++ site/engine_public/js/xsm_add_person_school.js
@@ -1,12 +1,17 @@
-$(document).ready(function() {
-
-    $('#member_person_id-selector').change(function() {
-        var id = $('#member_person_id-selector').val();
-        jQuery.get('/xsm/api/person/' + id.toString(), function(data) {
-            $('#department_id-selector').val(data.object.department_id);
-            $('#is_teacher-checkbox').prop('checked', data.object.is_teacher.length > 0);
-            $('#is_student-checkbox').prop('checked', data.object.is_student.length > 0);
-        });
+function xsm_set_selected_person_data()
+{
+    var id = $('#member_person_id-selector').val();
+    jQuery.get('/xsm/api/person/' + id.toString(), function(data) {
+        $('#department_id-selector').val(data.object.department_id);
+        $('#is_teacher-checkbox').prop('checked', data.object.is_teacher.length > 0);
+        $('#is_student-checkbox').prop('checked', data.object.is_student.length > 0);
     });
 
+}
+
+$(document).ready(function() {
+    // set initial values
+    xsm_set_selected_person_data();
+    // handle changes
+    $('#member_person_id-selector').change(xsm_set_selected_person_data);
 });
