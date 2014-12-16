da79d8c4f97ae80850c5aab75a67cce433c66a17
Mikhail Veltishchev <mvel@yandex-team.ru>
2014-12-17 01:09:05 +0300

    Fixed department selector refactoring bug

--- site/engine/cms/ank/department.php
+++ site/engine/cms/ank/department.php
@@ -1,10 +1,10 @@
 <?php
 
-function xsm_draw_department_selector($value, $id = 'department_id')
+function xsm_draw_department_selector($value, $name = 'department_id')
 {
     echo xsm_make_selector(
         'department',
-        'department_id',
+        $name,
         $value,
         array('department_title'),
         "(1 = 1) ORDER BY (department_id = 2), department_title"
