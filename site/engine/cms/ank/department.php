087876dc22c542807a76f3814ea91324a26ef85a
Mikhail Veltishchev <mvel@yandex-team.ru>
2014-12-17 09:45:20 +0300

    Department filter in person list

--- site/engine/cms/ank/department.php
+++ site/engine/cms/ank/department.php
@@ -1,13 +1,15 @@
 <?php
 
-function xsm_draw_department_selector($value, $name = 'department_id')
+function xsm_draw_department_selector($value, $name = 'department_id', $all = false)
 {
     echo xsm_make_selector(
         'department',
         $name,
         $value,
         array('department_title'),
-        "(1 = 1) ORDER BY (department_id = 2), department_title"
+        "(1 = 1) ORDER BY (department_id = 2), department_title",
+        "",
+        $all
     );
 }
 
