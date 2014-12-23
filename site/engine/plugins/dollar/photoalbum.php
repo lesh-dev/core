4decbccfd140800710f29d9486d80ee76d87fc51
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2014-12-23 12:04:41 +0300

    LOR: Photoalbum classes

--- site/engine/plugins/dollar/photoalbum.php
+++ site/engine/plugins/dollar/photoalbum.php
@@ -4,18 +4,15 @@
     $name = $_1;
     $album_text = $_2;
     $album_img_name = "$album_page_prefix/$name.jpg";
-    $album_arrow_name = "$album_page_prefix/arrow-$cl-small.png";
-?><img
-    class="album-<?php echo $cl; ?>"
-    src="<?php echo $album_img_name; ?>"
-    alt="<?php echo $name; ?>"
-    title="<?php echo $name; ?>"
-></img>
-<p class="album-<?php echo $cl; ?>">
-<img
-    class="album-arrow-<?php echo $cl; ?>"
-    src="<?php echo $album_arrow_name; ?>"
-    alt="<?php echo $cl; ?> arrow"
-></img>
-<?php echo $album_text; ?></p>
-<div style="clear: both;"></div>
+?>
+<div class="photoalbum-block">
+    <img
+        class="photoalbum-<?php echo $cl; ?>"
+        src="<?php echo $album_img_name; ?>"
+        alt="<?php echo $name; ?>"
+        title="<?php echo $name; ?>"
+    ></img>
+    <p class="photoalbum-<?php echo $cl; ?>">
+    <?php echo $album_text; ?></p>
+</div>
+<div class="photoalbum-div" style="clear: both;"></div>
