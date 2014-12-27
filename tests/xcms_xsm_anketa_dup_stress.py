f568a3a3a19f63c9752a4d0a624ad1341a6cde3f
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2014-12-27 14:40:10 +0300

    Some PEP8

--- tests/xcms_xsm_anketa_dup_stress.py
+++ tests/xcms_xsm_anketa_dup_stress.py
@@ -1,33 +1,35 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 
-import xtest_common, random_crap
+import xtest_common
+import random_crap
+
 
 class XcmsXsmAnketaDupStress(xtest_common.XcmsTest):
     """
     This test checks duplicate anketa merging algorithm.
     """
-    
+
     def generateData(self):
         self.inpLastName = u"Спамеров" + random_crap.randomText(4)
         self.inpFirstName = u"Егор" + random_crap.randomText(3)
         self.inpMidName = u"Федорович" + random_crap.randomText(2)
-        
-        self.inpBirthDate = random_crap.randomDigits(2) + "." + random_crap.randomDigits(2) + "." + random_crap.randomDigits(4);
-        
+
+        self.inpBirthDate = random_crap.randomDigits(2) + "." + random_crap.randomDigits(2) + "." + random_crap.randomDigits(4)
+
         self.inpSchool = u"Школа спамеров №" + random_crap.randomDigits(4)
-        
+
         self.inpSchoolCity = u"Спамерово-" + random_crap.randomDigits(2)
         self.inpClass = random_crap.randomDigits(1) + u" Жэ"
-        
+
         self.inpPhone = "+7" + random_crap.randomDigits(9)
         self.inpCell = "+7" + random_crap.randomDigits(9)
         self.inpEmail = random_crap.randomText(7) + "@" + random_crap.randomText(5) + ".ru"
-        
+
     def generateAuxData(self, iteration, random=False):
         self.inpSkype = random_crap.randomText(8)
         self.inpSocial = random_crap.randomVkontakte()
-              
+
         fmt = u"iteration {0}_{1}"
         if not random:
             fav = "my_favourites"
@@ -52,7 +54,7 @@ class XcmsXsmAnketaDupStress(xtest_common.XcmsTest):
         self.gotoUrlByLinkText(self.getEntranceLinkName())
         self.gotoAnketa()
         self.assertBodyTextPresent(self.getAnketaPageHeader())
-            
+
         # generate
         self.inpLastNameReal = self.fillElementById("last_name-input", self.inpLastName)
         self.inpFirstNameReal = self.fillElementById("first_name-input", self.inpFirstName)
@@ -67,26 +69,26 @@ class XcmsXsmAnketaDupStress(xtest_common.XcmsTest):
         self.inpAchReal = self.fillElementById("achievements-text", self.inpAch)
         self.inpHobReal = self.fillElementById("hobby-text", self.inpHob)
         self.inpSourceReal = self.fillElementById("lesh_ref-text", self.inpSource)
-        
+
         self.clickElementById("submit-anketa-button")
-    
+
     def checkUniqueAnketa(self):
-            
+
         # now login as admin
         self.personAlias = xtest_common.shortAlias(self.inpLastNameReal, self.inpFirstNameReal)
         # self.personAlias = xtest_common.fullAlias(self.inpLastNameReal, self.inpFirstNameReal, self.inpMidNameReal)
-        
+
         self.performLoginAsManager()
         self.gotoRoot()
         self.gotoUrlByLinkText(self.getAnketaListMenuName())
-        
+
         alias = self.fillElementById("show_name_filter-input", self.personAlias)
         self.clickElementByName("show-person")
         if self.countIndexedUrlsByLinkText(self.personAlias) != 1:
             self.failTest("Found more than one anketa with exact FIO. Duplicate filtering is broken. ")
         self.gotoRoot()
         self.performLogoutFromSite()
-                
+
     # -------------------- begining of the test
     def run(self):
         self.generateData()
@@ -98,5 +100,5 @@ class XcmsXsmAnketaDupStress(xtest_common.XcmsTest):
             else:
                 self.assertBodyTextNotPresent(self.getAnketaSuccessSubmitMessage())
                 self.assertBodyTextPresent(self.getAnketaDuplicateSubmitMessage())
-        
+
         self.checkUniqueAnketa()
