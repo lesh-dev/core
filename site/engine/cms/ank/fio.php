54f06e06bd87ec4e58c97c6f33ab226088264669
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2015-01-06 20:22:55 +0300

    Filtering by nick_name

--- site/engine/cms/ank/fio.php
+++ site/engine/cms/ank/fio.php
@@ -51,6 +51,39 @@ function xsm_fi_enc($person, $prefix = '')
     return htmlspecialchars(xsm_fi($person, $prefix));
 }
 
+/**
+  * Produce part of SQL WHERE expression for person filter
+  **/
+function xsm_person_name_filter($db, $show_name_filter)
+{
+    $show_name_filter = trim($show_name_filter);
+    if (!strlen($show_name_filter))
+        return '';
+
+    $words = explode(EXP_SP, $show_name_filter);
+    $cond = '';
+    foreach ($words as $word)
+    {
+        $w = trim($word);
+        if (!strlen($w))
+            continue;
+
+        if (strlen($cond))
+            $cond .= " AND ";
+        $esc_word = $db->escapeString($w);
+        $cond .=
+            "(".
+                "(last_name LIKE '%$esc_word%') OR ".
+                "(first_name LIKE '%$esc_word%') OR ".
+                "(patronymic LIKE '%$esc_word%') OR ".
+                "(nick_name LIKE '%$esc_word%')".
+            ")";
+    }
+    if (!strlen($cond))
+        return '';
+    return "($cond)";
+}
+
 function xsm_get_roles($obj)
 {
     // TODO: curatorship
