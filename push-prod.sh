7777f17d60d79867245bb5e91b5a2de3b79afd2b
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2015-06-20 09:14:31 +0300

    Better uberpushscript

--- push-prod.sh
+++ push-prod.sh
@@ -2,7 +2,14 @@
 
 echo -n "Source version: "
 cat site/VERSION
+echo "Last tags:"
+git tag | sort -V | tail -n 5
+echo "Pushing to github..."
 git push
+echo "    and tags..."
 git push --tags
+echo "Pushing to dev.fizlesh.ru..."
 git push ssh://dev.fizlesh.ru//srv/git/lesh
+echo "    and tags..."
 git push --tags ssh://dev.fizlesh.ru//srv/git/lesh
+echo "Everything completed"
