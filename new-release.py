9979c1ecdf6363e5fc82fb4d0c16237db66d48f5
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2015-06-23 22:50:16 +0300

    Added release script

---
+++ new-release.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import datetime
+import os
+
+version_file = 'site/VERSION'
+
+current_version = open(version_file).read().strip()
+version_components = current_version.split('.')
+version_components[2] = str(int(version_components[2]) + 1)
+new_version = '.'.join(version_components)
+
+print "New version is", new_version
+with open(version_file, 'w') as vf:
+    vf.write(new_version + '\n')
+os.system('git commit -m "Version updated to {}" {}'.format(new_version, version_file))
+os.system('git tag {}-prod-{}'.format(new_version, datetime.datetime.now().strftime("%Y.%m.%d")))
