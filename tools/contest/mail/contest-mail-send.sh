8318e48bb36aa712a1f8d3101fbf0012110e58f5
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2015-11-08 16:55:30 +0300

    Update mailsend

--- tools/contest/mail/contest-mail-send.sh
+++ tools/contest/mail/contest-mail-send.sh
@@ -12,10 +12,10 @@ echo "Sending contest from $mail_list"
 for addr in $(cat $mail_list); do
     mutt \
         -F muttrc \
-        -a pravila-olimpiada-fizlesh-contest-2014-2015.pdf \
-        -a usloviya-olimpiada-fizlesh-contest-2014-2015.pdf \
-        -s 'О Межрегиональной Физической Олимпиаде 2014-2015' \
-        -- $addr < letter2014.txt
+        -a pravila-olimpiada-fizlesh-contest-2015-2016.pdf \
+        -a usloviya-olimpiada-fizlesh-contest-2015-2016.pdf \
+        -s 'О Межрегиональной Физической Олимпиаде 2015-2016' \
+        -- $addr < letter2015.txt
     sleep 1
     echo "Sent to $addr"
 done
