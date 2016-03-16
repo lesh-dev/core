2b0e170da760e4f5125124b77d0598addbabbf13
Natasha Skrobova <nata-skr@yandex.ru>
2016-03-16 21:02:36 +0300

    Issue #902 resolved (doxy comments test added)

--- tests/auto_test_set.py
+++ tests/auto_test_set.py
@@ -15,6 +15,7 @@ import xcms_auth_set_dup_email_by_admin
 import xcms_content_add_page
 import xcms_content_auth_only_page
 import xcms_content_bad_path_page
+import xcms_content_comment_processing
 import xcms_content_contlists
 import xcms_content_dollar_plugin
 import xcms_content_hidden_page
@@ -66,6 +67,7 @@ def getTests(baseUrl, args):
         ("xcms_content_add_page.py", xcms_content_add_page.XcmsContentAddPage(baseUrl, args)),
         ("xcms_content_auth_only_page.py", xcms_content_auth_only_page.XcmsContentAuthOnlyPage(baseUrl, args)),
         ("xcms_content_bad_path_page.py", xcms_content_bad_path_page.XcmsContentBadPathPage(baseUrl, args)),
+        ("xcms_content_comment_processing.py", xcms_content_comment_processing.XcmsContentCommentProcessing(baseUrl, args)),
         ("xcms_content_contlists.py", xcms_content_contlists.XcmsContentContlist(baseUrl, args)),
         ("xcms_content_dollar_plugin.py", xcms_content_dollar_plugin.XcmsContentDollarPlugin(baseUrl, args)),
         ("xcms_content_hidden_page.py", xcms_content_hidden_page.XcmsContentHiddenPage(baseUrl, args)),
---
+++ tests/xcms_content_comment_processing.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+import xtest_common
+import random_crap
+import os
+
+class XcmsContentCommentProcessing(xtest_common.XcmsTest):
+    
+
+    def run(self):
+        
+        self.performLoginAsAdmin()
+        self.gotoAdminPanel()
+
+        self.m_parentPage = u"Главная"
+
+        self.gotoUrlByLinkText(self.m_parentPage)
+        self.gotoCreatePage()
+
+        inpPageDir = "test_page_" + random_crap.randomText(8)
+        inpMenuTitle = "menu_title_" + random_crap.randomText(8)
+        inpPageHeader = "page_header_" + random_crap.randomText(8)
+        inpAlias = "new/page/alias/" + random_crap.randomText(8)
+
+        inpPageDir = self.fillElementById("create-name-input", inpPageDir)
+        inpMenuTitle = self.fillElementById("menu-title-input", inpMenuTitle)
+        inpPageHeader = self.fillElementById("header-input", inpPageHeader)
+        inpAlias = self.fillElementById("alias-input", inpAlias)
+
+        self.m_pageAlias = inpAlias
+
+        defaultPageType = self.getOptionValueById("create-pagetype-selector")
+
+        if defaultPageType != "content":
+            self.failTest("Default selected page type is not 'content': " + defaultPageType)
+
+        self.clickElementById("create-page-submit")
+
+        #self.logAdd("Opening editor again after redirection. ")
+        #self.gotoEditPageInPlace()
+
+        self.m_menuTitle = inpMenuTitle
+        self.m_pageHeader = inpPageHeader
+
+        # edit page - click on menu
+        self.gotoUrlByLinkText(inpMenuTitle)
+        
+        pageText = "<?php\n/**\n* qqq\n*\n**/\n?>"
+        #print "Generated page text: '" + pageText + "'"
+
+        pageText = self.fillAceEditorElement(pageText)
+        #print "After ins page text: '" + pageText + "'"
+        self.clickElementById("edit-submit-top")
+        
+        self.performLogoutFromAdminPanel()
+        
+        self.gotoUrlByLinkText(inpMenuTitle)
+        
+        self.assertPhpErrors() 
