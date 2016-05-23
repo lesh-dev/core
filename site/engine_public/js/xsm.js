a7633ac6e53fed541f217d532f7d10ff9cd13cdb
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2016-05-24 01:03:59 +0300

    Enum api works, but still does not edit

--- site/engine_public/js/xsm.js
+++ site/engine_public/js/xsm.js
@@ -15,6 +15,15 @@ function xsm_filter_form_autosubmit()
 }
 
 
+function _find_row(ele)
+{
+    while (ele && ele.prop('nodeName') != 'TR') {
+        ele = ele.parent();
+    }
+    return ele;
+}
+
+
 function _xsm_edit_field_keypress_handler(event)
 {
     if (event.which == 13)
@@ -26,12 +35,8 @@ function _xsm_edit_field_keypress_handler(event)
         field_name = cell.attr('field-name')
         data = {}
         data[field_name] = new_content;
-        var ele = cell;
-        while (ele && ele.prop('nodeName') != 'TR') {
-            ele = ele.parent();
-        }
 
-        id = ele.attr('row-id');
+        var id = _find_row(cell).attr('row-id');
         json_data = JSON.stringify(data);
         encoded_data = encodeURIComponent(json_data);
         jQuery.get('/xsm/api/update/person/' + id + '/' + encoded_data, function(data) {
@@ -46,20 +51,50 @@ function _xsm_edit_field_keypress_handler(event)
 
 function xsm_edit_field_handler(event)
 {
-    var original_content = $(this).text();
-    $(this).addClass("cell-editing");
-    $(this).html('<input type="text"/>');
-    var input = $(this).children().first();
-    input.focus();
-    input.keypress(_xsm_edit_field_keypress_handler);
-    input.val(original_content);
+    var cell = $(this);
+    field_name = cell.attr('field-name')
+    var enum_type = cell.attr('enum-type')
+    var id = _find_row(cell).attr('row-id');
 
-    // closure is here to original content (it's better to save it via data())
-    input.blur(function() {
-        var cell = $(this).parent();
-        cell.text(original_content);
-        cell.removeClass("cell-editing");
+    jQuery.get('/xsm/api/get/person/' + id, function(data) {
+        var original_content = data["object"][field_name];
+        cell.addClass("cell-editing");
+
+        if (enum_type)
+        {
+            // enum
+            jQuery.get('/xsm/api/enum_html/' + field_name + '/' + original_content + '/' + enum_type, function(enum_data) {
+                cell.html(enum_data.enum);
+                var input = cell.children().first();
+                input.focus();
+
+                // closure is here to original content (it's better to save it via data())
+                input.blur(function() {
+                    cell.text(original_content);
+                    cell.removeClass("cell-editing");
+                });
+
+            });
+
+        }
+        else
+        {
+            // text control
+            cell.html('<input type="text"/>');
+            var input = cell.children().first();
+            input.focus();
+            input.keypress(_xsm_edit_field_keypress_handler);
+            input.val(original_content);
+
+            // closure is here to original content (it's better to save it via data())
+            input.blur(function() {
+                cell.text(original_content);
+                cell.removeClass("cell-editing");
+            });
+        }
     });
+
+    // finish event
     event.stopPropagation();
     return false;
 }
