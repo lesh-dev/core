8dbf8923862f8fb99d139abe2d17dde22de1c6c8
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2016-10-13 20:06:12 +0300

    [tests] PEP8 terrorism

--- tests/xcms_xsm_add_exams.py
+++ tests/xcms_xsm_add_exams.py
@@ -20,44 +20,44 @@ class XcmsXsmAddExams(xtest_common.XcmsTest):
     listened_status = None
 
     @staticmethod
-    def getExamAlreadyExistsMessage():
+    def get_exam_already_exists_message():
         return u"Зачёт по этому курсу уже имеется"
 
-    def addExamsById(self, examIdList):
+    def add_exams_by_id(self, examIdList):
         for exam in examIdList:
-            self.addExamByIdAndReturn(exam)
+            self.add_exam_by_id_and_return(exam)
 
-    def addExamByIdAndReturn(self, exam):
-        self.addExamById(exam)
-        self.assertBodyTextNotPresent(self.getExamAlreadyExistsMessage())
+    def add_exam_by_id_and_return(self, exam):
+        self.add_exam_by_id(exam)
+        self.assertBodyTextNotPresent(self.get_exam_already_exists_message())
         self.gotoBackToPersonView()
 
-    def addExamById(self, exam):
+    def add_exam_by_id(self, exam):
         self.gotoUrlByLinkText(u"Добавить зачёт")
         self.setOptionValueByIdAndValue("course_id-selector", exam)
         self.clickElementByName("update-exam")
 
-    def setExamPassed(self, examLineList):
-        for examLine in examLineList:
+    def set_exam_passed(self, exam_line_list):
+        for examLine in exam_line_list:
             # <a><span>Прослушан</span></a>
             self.gotoIndexedUrlByLinkText(self.listened_status, examLine, "span")
             self.setOptionValueByIdAndValue("exam_status-selector", "passed")
 
-            examComment = u"Коммент к сданному зачёту: " + random_crap.randomText(6)
-            self.fillElementByName("exam_comment", examComment)
+            exam_comment = u"Коммент к сданному зачёту: " + random_crap.randomText(6)
+            self.fillElementByName("exam_comment", exam_comment)
             self.clickElementByName("update-exam")
             self.gotoBackToPersonView()
 
         self.assertBodyTextPresent(u"Сдан")
 
-    def setExamNotPassed(self, examLineList):
-        for examLine in examLineList:
+    def set_exam_not_passed(self, exam_line_list):
+        for examLine in exam_line_list:
             # <a><span>Прослушан</span></a>
             self.gotoIndexedUrlByLinkText(self.listened_status, examLine, "span")
             self.setOptionValueByIdAndValue("exam_status-selector", "notpassed")
 
-            examComment = u"Коммент к несданному зачёту: " + random_crap.randomText(6)
-            self.fillElementByName("exam_comment", examComment)
+            exam_comment = u"Коммент к несданному зачёту: " + random_crap.randomText(6)
+            self.fillElementByName("exam_comment", exam_comment)
             self.clickElementByName("update-exam")
             self.gotoBackToPersonView()
 
@@ -90,24 +90,24 @@ class XcmsXsmAddExams(xtest_common.XcmsTest):
         self.clickElementByName("update-person_school")
         self.gotoBackToPersonView()
 
-        #<option  value="95">Базовое электричество &#8212; Тараненко Сергей</option>
-        #<option  value="134">Биомеханика &#8212; Преподаватель Другого</option>
-        #<option  value="83">Ботаника &#8212; Преподаватель Другого</option>
-        #<option  value="73">Введение в технику эксперимента &#8212; Пюрьбеева Евгения</option>
-        #<option  value="119">Введение в химию &#8212; Марьясина Софья</option>
-        #<option  value="101">Видеосъёмка &#8212; Дюно-Люповецкий Влас</option>
-        #<option  value="137">Генетические алгоритмы &#8212; Мироненко-Маренков Антон</option>
-        #<option  value="91">Геометрическая оптика &#8212; Пилипюк Дарья</option>
+        # <option  value="95">Базовое электричество &#8212; Тараненко Сергей</option>
+        # <option  value="134">Биомеханика &#8212; Преподаватель Другого</option>
+        # <option  value="83">Ботаника &#8212; Преподаватель Другого</option>
+        # <option  value="73">Введение в технику эксперимента &#8212; Пюрьбеева Евгения</option>
+        # <option  value="119">Введение в химию &#8212; Марьясина Софья</option>
+        # <option  value="101">Видеосъёмка &#8212; Дюно-Люповецкий Влас</option>
+        # <option  value="137">Генетические алгоритмы &#8212; Мироненко-Маренков Антон</option>
+        # <option  value="91">Геометрическая оптика &#8212; Пилипюк Дарья</option>
 
         self.assertBodyTextPresent(u"Зачёты")
 
-        dupId = 134
+        dup_id = 134
 
-        self.addExamsById([95, 119, 91, 73, 107, 130, 133, dupId])
+        self.add_exams_by_id([95, 119, 91, 73, 107, 130, 133, dup_id])
 
-        self.setExamPassed([1, 2, 2])
-        self.setExamNotPassed([1, 2, 2])
+        self.set_exam_passed([1, 2, 2])
+        self.set_exam_not_passed([1, 2, 2])
 
         # test duplicate exam
-        self.addExamById(dupId)
-        self.assertBodyTextPresent(self.getExamAlreadyExistsMessage())
+        self.add_exam_by_id(dup_id)
+        self.assertBodyTextPresent(self.get_exam_already_exists_message())
