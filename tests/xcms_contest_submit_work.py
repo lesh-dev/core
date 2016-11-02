bb6cb732cec72b0b79d26e91165bd65d9ee16764
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2016-11-02 12:47:17 +0300

    [contest] Fix js loading sequence

--- tests/xcms_contest_submit_work.py
+++ tests/xcms_contest_submit_work.py
@@ -10,7 +10,7 @@ class XcmsContestSubmitWork(xtest_common.XcmsTest):
     """
     This test checks contest work uploading.
     Steps:
-    * login as ???
+    * login as ??? (now as admin, but contest manager should be there)
     * goto contest management page
     * add new work
     * check if it was really loaded
@@ -18,23 +18,24 @@ class XcmsContestSubmitWork(xtest_common.XcmsTest):
 
     def run(self):
         self.ensure_logged_off()
+        # TODO(mvel): Add contest manager role
         self.performLoginAsAdmin()
         self.gotoPage("/ctx")
 
         self.gotoUrlByLinkText(u"Добавить работу")
 
-        inpFIO = u"Участник_Олимпиады_" + random_crap.random_text(6)
-        inpEmail = random_crap.randomEmail()
-        inpComment = random_crap.random_text(6)
+        inp_fio = u"Участник_Олимпиады_" + random_crap.random_text(6)
+        inp_email = random_crap.randomEmail()
+        inp_comment = random_crap.random_text(6)
 
-        workFile = os.getcwd() + "/contest-work-sample.png"
-        inpFIO = self.fillElementByName("name", inpFIO)
-        inpEmail = self.fillElementByName("mail", inpEmail)
-        workFile = self.fillElementByName("work", workFile, clear=False)
+        work_file = os.getcwd() + "/contest-work-sample.png"
+        inp_fio = self.fillElementByName("name", inp_fio)
+        _ = self.fillElementByName("mail", inp_email)
+        _ = self.fillElementByName("work", work_file, clear=False)
         # WTF??? Why 'status', not 'comment'?
-        inpComment = self.fillElementByName("status", inpComment)
+        _ = self.fillElementByName("status", inp_comment)
         self.clickElementByName("ctx_add_or_edit_form_contestants_x")
 
         # check that work appears in the list
         self.gotoUrlByLinkText(u"Работы")
-        self.assertBodyTextPresent(inpFIO)
+        self.assertBodyTextPresent(inp_fio)
