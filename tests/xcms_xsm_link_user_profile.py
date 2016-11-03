4f06572602ac4cc6b1abb92fd002171bee66d714
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2016-11-03 22:58:48 +0300

    [tests] Less code

--- tests/xcms_xsm_link_user_profile.py
+++ tests/xcms_xsm_link_user_profile.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 
+import xsm
 import xtest_common
 import random_crap
 
@@ -16,13 +17,13 @@ class XcmsXsmLinkUserProfile(xtest_common.XcmsTest):
 
     def run(self):
         self.ensure_logged_off()
-        inpLogin = "xsm_link_" + random_crap.random_text(6)
-        inpEMail = random_crap.randomEmail()
-        inpPass = random_crap.random_text(8)
-        inpName = u"XSM-Юзер-" + random_crap.random_text(6)
+        inp_login = "xsm_link_" + random_crap.random_text(6)
+        inp_email = random_crap.randomEmail()
+        inp_pass = random_crap.random_text(8)
+        inp_name = u"XSM-Юзер-" + random_crap.random_text(6)
 
-        inpLogin, inpEMail, inpPass, inpName = self.createNewUser(
-            inpLogin, inpEMail, inpPass, inpName,
+        inp_login, inp_email, inp_pass, inp_name = self.createNewUser(
+            inp_login, inp_email, inp_pass, inp_name,
             aux_params=["do_not_logout_admin", "manager_rights"]
         )
 
@@ -31,37 +32,27 @@ class XcmsXsmLinkUserProfile(xtest_common.XcmsTest):
         self.gotoXsmActive()
         self.gotoXsmAddPerson()
 
-        # generate
-        inpLastName = u"ИксЭсЭмов" + random_crap.random_text(4)
-        inpFirstName = u"Юзер_" + random_crap.random_text(3)
-        inpMidName = u"Ламерович_" + random_crap.random_text(3)
-        inpEMailXsm = inpEMail
-
-        inpLastName = self.fillElementById("last_name-input", inpLastName)
-        inpFirstName = self.fillElementById("first_name-input", inpFirstName)
-        inpMidName = self.fillElementById("patronymic-input", inpMidName)
-        inpEMailXsm = self.fillElementById("email-input", inpEMailXsm)
-
-        if inpEMail != inpEMailXsm:
-            self.failTest("Cannot create user with identical email. ")
-
-        self.clickElementById("update-person-submit")
+        person = xsm.Person(self)
+        person.input(
+            last_name=u"ИксЭсЭмов",
+            first_name=u"Юзер",
+            patronymic=u"Ламерович",
+            email=inp_email,
+        )
 
         self.gotoRoot()
         self.performLogoutFromSite()
 
-        if not self.performLogin(inpLogin, inpPass):
+        if not self.performLogin(inp_login, inp_pass):
             self.failTest("Cannot login as newly created user. ")
 
         self.gotoCabinet()
         self.assertBodyTextPresent("XSM")
-        cardCaption = u"Карточка участника в XSM"
-        self.assertBodyTextPresent(cardCaption)
-        xsmUrlText = xtest_common.shortAlias(inpLastName, inpFirstName)
-        self.gotoUrlByLinkText(xsmUrlText)
-        xsmAlias = xtest_common.fullAlias(inpLastName, inpFirstName, inpMidName)
-        #self.checkPersonAliasInPersonView(xsmAlias, "We should get into our XSM person card. ")
-        self.assertBodyTextPresent(xsmAlias, "We should get into our XSM person card. ")
+        card_caption = u"Карточка участника в XSM"
+        self.assertBodyTextPresent(card_caption)
+        xsm_url_text = person.short_alias()
+        self.gotoUrlByLinkText(xsm_url_text)
+        self.assertBodyTextPresent(person.full_alias(), "We should get into our XSM person card. ")
 
         self.gotoRoot()
         self.performLogoutFromSite()
@@ -70,11 +61,9 @@ class XcmsXsmLinkUserProfile(xtest_common.XcmsTest):
         self.gotoAdminPanel()
         self.gotoUserList()
 
-        self.gotoUrlByPartialLinkText(inpLogin, "Click on user name in the list")
+        self.gotoUrlByPartialLinkText(inp_login, "Click on user name in the list")
 
-        self.assertBodyTextPresent(cardCaption, "We should see XSM card link. ")
-        xsmLinkAlias = xtest_common.shortAlias(inpLastName, inpFirstName)
-
-        self.gotoUrlByLinkText(xsmLinkAlias, "Click on XSM card link")
+        self.assertBodyTextPresent(card_caption, "We should see XSM card link. ")
+        self.gotoUrlByLinkText(person.short_alias(), "Click on XSM card link")
 
         # TODO: here also should be link to user's XSM profile, if any.
