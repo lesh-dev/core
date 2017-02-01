9e627d79bdb7f20c212ccaaad6c8e78215a52bb9
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2017-02-01 12:57:51 +0300

    Break photoalbum and resolve #941.
    Now ${staffimg...} dollar plugin exploits standard page files mechanism.
    This means that all content from production /portraits/ folder should be
    moved and appropriate metadata generated.

--- site/engine/plugins/dollar/staffimg.php
+++ site/engine/plugins/dollar/staffimg.php
@@ -1,5 +1,6 @@
 <?php
     $staff_page_prefix = "${full_content_dir}cms/pages/$pageid";
-    $img_name = "${staff_page_prefix}/portraits/$_0";
+    $img_name = "${staff_page_prefix}/$_0";
     ?><a class="portrait" href="<?php echo $img_name; ?>.jpg"
-        target="_blank"><img src="<?php echo $img_name; ?>-preview.jpg" alt="<?php echo $_1; ?>"></img></a>
+        target="_blank"><img src="<?php echo $img_name; ?>-preview.jpg"
+        alt="<?php echo $_1; ?>"></img></a>
