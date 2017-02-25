ef0d10c6931a280f7a6002a6a90c59c675f1affb
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2017-02-25 12:23:31 +0300

    Fix tests after refactoring

--- tests/xcms_xsm_hack_person_link.py
+++ tests/xcms_xsm_hack_person_link.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 
+import xsm
 import xtest_common
 
-class XcmsXsmHackPersonLink(xtest_common.XcmsTest):
+
+class XcmsXsmHackPersonLink(xsm.Manager, xtest_common.XcmsTest):
     """
     This test checks person phone parsing feature
     It does following:
-    * logins as xsm manager
-    * navigated malformed person link
+    * Logs in as XSM manager
+    * Navigates malformed person link
     """
 
     def run(self):
@@ -20,4 +22,3 @@ class XcmsXsmHackPersonLink(xtest_common.XcmsTest):
         self.gotoPage("/xsm/view-person&person_id=286")
         self.gotoPage("/xsm/view-person&person_id=far_away")
         self.assertBodyTextPresent(u"Объект")
-        
