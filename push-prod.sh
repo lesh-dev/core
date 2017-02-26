826853b6a84ceba399152ca1addfac5d5dba78d9
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2017-02-26 08:31:13 +0300

    Fix

--- push-prod.sh
+++ push-prod.sh
@@ -8,8 +8,8 @@ echo "Pushing to github..."
 git push
 echo "    and tags..."
 git push --tags
-echo "Pushing to dev.fizlesh.ru..."
-git push ssh://dev.fizlesh.ru//srv/git/lesh
+echo "Pushing to fizlesh.ru..."
+git push ssh://fizlesh.ru//srv/git/lesh
 echo "    and tags..."
-git push --tags ssh://dev.fizlesh.ru//srv/git/lesh
+git push --tags ssh://fizlesh.ru//srv/git/lesh
 echo "Everything completed"
