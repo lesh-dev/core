143ed884ee371e6d871fbc5f413d4793eb3c69f8
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2017-10-11 13:57:44 +0300

    Use installer library

--- push_prod.sh
+++ push_prod.sh
@@ -1,33 +1,35 @@
 #!/usr/bin/env bash
 
-echo -n "Source version: "
-cat version
-echo "Last tags:"
+. deploy-tools/installer/installer.sh
+
+version="$(echo -n "Source version: " && cat version)"
+print_message $version
+print_message "Last tags:"
 git tag | sort -Vr | head -n 5
 
 gh_lesh="git@github.com:lesh-dev/core.git"
 vm_root="ssh://fizlesh.ru//srv/git"
 bb_root="ssh://hg@bitbucket.org/dichlofos"
 
-echo "Pushing to github..."
-echo "    [repo] lesh..."
+print_message "Pushing to github..."
+print_message "    [repo] lesh..."
 git push $gh_lesh
-echo "    [tags] lesh..."
+print_message "    [tags] lesh..."
 git push --tags $gh_lesh
-echo "    [repo] deploy-tools"
+print_message "    [repo] deploy-tools"
 (cd deploy-tools && hg push $bb_root/deploy-tools )
-echo "    [repo] xengine"
+print_message "    [repo] xengine"
 (cd site/xengine && hg push $bb_root/xengine )
 
-echo "Pushing to fizlesh.ru..."
-echo "    [repo] lesh..."
+print_message "Pushing to fizlesh.ru..."
+print_message "    [repo] lesh..."
 git push $vm_root/lesh
-echo "    [tags] lesh..."
+print_message "    [tags] lesh..."
 git push --tags $vm_root/lesh
 
-echo "    [repo] deploy-tools"
+print_message "    [repo] deploy-tools"
 ( cd site/xengine && hg push $vm_root/xengine )
-echo "    [repo] xengine"
+print_message "    [repo] xengine"
 ( cd deploy-tools && hg push $vm_root/deploy-tools )
 
-echo "Everything completed"
+print_message "Everything completed"
