1c570ffcadb15e349b8763fc5c9f76287837de05
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2017-10-11 13:54:47 +0300

    Push all to prod

--- push_prod.sh
+++ push_prod.sh
@@ -4,12 +4,30 @@ echo -n "Source version: "
 cat version
 echo "Last tags:"
 git tag | sort -Vr | head -n 5
+
+gh_lesh="git@github.com:lesh-dev/core.git"
+vm_root="ssh://fizlesh.ru//srv/git"
+bb_root="ssh://hg@bitbucket.org/dichlofos"
+
 echo "Pushing to github..."
-git push
-echo "    and tags..."
-git push --tags
+echo "    [repo] lesh..."
+git push $gh_lesh
+echo "    [tags] lesh..."
+git push --tags $gh_lesh
+echo "    [repo] deploy-tools"
+(cd deploy-tools && hg push $bb_root/deploy-tools )
+echo "    [repo] xengine"
+(cd site/xengine && hg push $bb_root/xengine )
+
 echo "Pushing to fizlesh.ru..."
-git push ssh://fizlesh.ru//srv/git/lesh
-echo "    and tags..."
-git push --tags ssh://fizlesh.ru//srv/git/lesh
+echo "    [repo] lesh..."
+git push $vm_root/lesh
+echo "    [tags] lesh..."
+git push --tags $vm_root/lesh
+
+echo "    [repo] deploy-tools"
+( cd site/xengine && hg push $vm_root/xengine )
+echo "    [repo] xengine"
+( cd deploy-tools && hg push $vm_root/deploy-tools )
+
 echo "Everything completed"
