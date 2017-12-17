42149641b1e9e490bdfd9723459698e12fdb8abe
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2017-12-17 15:09:14 +0300

    Fix tezts

--- tests/xcms_content_remove_page.py
+++ tests/xcms_content_remove_page.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 
+import xpage
 import xtest_common
 import random_crap
 
@@ -20,26 +21,22 @@ class XcmsContentRemovePage(xtest_common.XcmsTest):
         self.performLoginAsEditor()
         self.gotoAdminPanel()
 
-        self.m_parentPage = u"Главная"
+        self.parent_page = u"Главная"
 
-        self.gotoUrlByLinkText(self.m_parentPage)
+        self.gotoUrlByLinkText(self.parent_page)
         self.gotoCreatePage()
 
-        inpPageDir = "phoenix_" + random_crap.random_text(8)
-        inpMenuTitle = "phoenix_menu_title_" + random_crap.random_text(8)
-        inpPageHeader = "phoenix_header_" + random_crap.random_text(8)
-        inpAlias = "removed/phoenix/alias/" + random_crap.random_text(8)
-
-        inpPageDir = self.fillElementById("create-name-input", inpPageDir)
-        inpMenuTitle = self.fillElementById("menu-title-input", inpMenuTitle)
-        inpPageHeader = self.fillElementById("header-input", inpPageHeader)
-        inpAlias = self.fillElementById("alias-input", inpAlias)
-
-        self.clickElementById("create-page-submit")
+        page = xpage.Page(self)
+        page.input(
+            page_dir="phoenix_",
+            menu_title="phoenix_menu_title_",
+            header="phoenix_header_",
+            alias="removed/phoenix/alias/",
+            random=True,
+        )
 
         # edit page - click on menu
-        self.gotoUrlByLinkText(inpMenuTitle)
-
+        self.gotoUrlByLinkText(page.menu_title)
         pageText = random_crap.randomCrap(10)
         pageText = self.fillAceEditorElement(pageText)
         self.clickElementById("commit-submit")
@@ -47,39 +44,42 @@ class XcmsContentRemovePage(xtest_common.XcmsTest):
         self.gotoCloseEditor()
         self.gotoAdminPanel()
 
-        self.gotoUrlByLinkText(self.m_parentPage)
-        self.gotoUrlByLinkText(inpMenuTitle)
+        self.gotoUrlByLinkText(self.parent_page)
+        self.gotoUrlByLinkText(page.menu_title)
 
         self.gotoRemovePage()
         self.assertBodyTextPresent(u"Удаление страницы")
         self.clickElementById("delete_page-submit")
 
         self.gotoCloseEditor()
-        self.gotoUrlByLinkText(self.m_parentPage)
-        self.assertUrlNotPresent(inpMenuTitle, "page should be removed")
+        self.gotoUrlByLinkText(self.parent_page, attribute=self.CONTENT)
+        if self.get_url_by_link_data(page.menu_title, attribute=self.CONTENT, fail=False) is not None:
+            self.failTest("Page should be removed")
 
         # create same page again
         self.gotoAdminPanel()
-        self.gotoUrlByLinkText(self.m_parentPage)
+        self.gotoUrlByLinkText(self.parent_page)
         self.gotoCreatePage()
 
-        inpPageDir = self.fillElementById("create-name-input", inpPageDir)
-        inpMenuTitle = self.fillElementById("menu-title-input", inpMenuTitle)
-        inpPageHeader = self.fillElementById("header-input", inpPageHeader)
-        inpAlias = self.fillElementById("alias-input", inpAlias)
-
-        self.clickElementById("create-page-submit")
-
-        self.gotoUrlByLinkText(inpMenuTitle)
+        page.input(
+            page_dir=page.page_dir,
+            menu_title=page.menu_title,
+            header=page.header,
+            alias=page.alias,
+        )
+        self.gotoUrlByLinkText(page.menu_title)
 
         newPageText = random_crap.randomCrap(10)
         newPageText = self.fillAceEditorElement(pageText)
         self.clickElementById("commit-submit")
         self.gotoCloseEditor()
 
-        self.gotoUrlByLinkText(self.m_parentPage)
-        self.gotoUrlByLinkText(inpMenuTitle)
+        self.gotoUrlByLinkText(self.parent_page, attribute=self.CONTENT)
+        self.gotoUrlByLinkText(page.menu_title, attribute=self.CONTENT)
 
-        self.assertElementTextById("content-text", newPageText, "new page text should appear after page reincarnation. ")
+        self.assertElementTextById(
+            "content-text", newPageText,
+            "new page text should appear after page reincarnation. "
+        )
 
-        self.goto_alias(inpAlias)
+        self.goto_alias(page.alias)
