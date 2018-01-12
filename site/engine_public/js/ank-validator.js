f452deffee840043c2841a0d8e2c8394e1ab6b9e
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2018-01-12 03:27:22 +0300

    Med submission passes js validation

--- site/engine_public/js/ank-validator.js
+++ site/engine_public/js/ank-validator.js
@@ -1,17 +1,30 @@
-function xsm_ank_check_name(aError, sFieldName, sFieldTitle) {
-    var val = $("#" + sFieldName + "-input").val();
+/**
+ * My own jQuery
+ * @param {*} element_name html element name
+ * @param {*} element_type html element type, input by default
+ */
+function $xsm(element_name, element_type) {
+    if (!element_type) {
+        element_type = "input";
+    }
+    return $("#" + element_name + "-" + element_type);
+}
+
+
+function xsm_ank_check_name(errors, sFieldName, sFieldTitle) {
+    var val = $xsm(sFieldName).val();
     val = $.trim(val);
     var valLenTest = val.replace(/[^a-zA-Zа-яА-Я]/g, '');
     if (valLenTest.length < 2) {
-        aError.push("Поле '" + sFieldTitle + "' слишком короткое");
+        errors.push("Поле '" + sFieldTitle + "' слишком короткое");
         return false;
     }
     if (valLenTest.length > 80) {
-        aError.push("Поле '" + sFieldTitle + "' слишком длинное");
+        errors.push("Поле '" + sFieldTitle + "' слишком длинное");
         return false;
     }
     if (/[\/.,?!@#$%&*()_+=~^]/.test(val)) {
-        aError.push("Поле '" + sFieldTitle + "' должно содержать только русские и английские символы");
+        errors.push("Поле '" + sFieldTitle + "' должно содержать только русские и английские символы");
         return false;
     }
     return true;
@@ -21,17 +34,17 @@ function xsm_ank_on_change() {
     GShowWarning = true;
 }
 
-function xsm_ank_check_class(aError) {
-    var val = $("#ank_class-input").val();
+function xsm_ank_check_class(errors) {
+    var val = $xsm("ank_class").val();
     val = $.trim(val);
-    if (val.length < 1) aError.push("Класс не указан");
+    if (val.length < 1) errors.push("Класс не указан");
 }
 
-function xsm_ank_check_birth_date(aError, sFieldName, sFieldTitle) {
-    var val = $("#" + sFieldName + "-input").val();
+function xsm_ank_check_birth_date(errors, sFieldName, sFieldTitle) {
+    var val = $xsm(sFieldName).val();
     val = $.trim(val);
     if (!/\d\d.\d\d.\d\d\d\d/.test(val)) {
-        aError.push("Поле '" + sFieldTitle + "' должно иметь формат ДД.ММ.ГГГГ");
+        errors.push("Поле '" + sFieldTitle + "' должно иметь формат ДД.ММ.ГГГГ");
         return false;
     }
     return true;
@@ -42,8 +55,8 @@ function xsm_ank_check_birth_date(aError, sFieldName, sFieldTitle) {
  * @param element_name: non-default element name to check
  */
 function xsm_ank_check_email(errors, element_name) {
-    var element_id = (element_name) ? ('#' + element_name + "-input") : "#email-input";
-    var val = $(element_name).val();
+    var element = (element_name) ? $xsm(element_name) : $xsm("email");
+    var val = element.val();
     val = $.trim(val);
     if (val.length && val.indexOf('@') < 0) {
         errors.push("EMail должен содержать знак '@'");
@@ -51,7 +64,7 @@ function xsm_ank_check_email(errors, element_name) {
 }
 
 function xsm_ank_check_control_question(errors, mode) {
-    var val = $("#control_question-input").val();
+    var val = $xsm("control_question").val();
     val = $.trim(val);
     if (mode == "fizlesh") {
         if (!(
@@ -75,21 +88,25 @@ function xsm_ank_check_control_question(errors, mode) {
     }
 }
 
-function xsm_ank_check_phone(id, nDigit) {
-    var val = $(id).val();
+function xsm_ank_check_phone(element_name, min_digit_count) {
+    var val = $xsm(element_name).val();
     val = $.trim(val);
-    var valLenTest = val.replace(/[^0-9]/g, '');
-    if (valLenTest.length < nDigit)
-        return "телефон должен содержать не меньше " + nDigit + " цифр. ";
+    var val_length_test = val.replace(/[^0-9]/g, '');
+    if (val_length_test.length < min_digit_count) {
+        return "телефон должен содержать не меньше " + min_digit_count + " цифр. ";
+    }
     return '';
 }
 
-function xsm_ank_check_phones(aError) {
-    var sPhError = xsm_ank_check_phone("#phone-input", 7);
-    var sCellError = xsm_ank_check_phone("#cellular-input", 9);
-    if (sPhError.length && sCellError.length)
-        aError.push("Укажите правильно хотя бы один из телефонов! Домашний " +
-            sPhError + "Мобильный " + sCellError);
+function xsm_ank_check_phones(errors) {
+    var phone_error = xsm_ank_check_phone("phone", 7);
+    var cellular_error = xsm_ank_check_phone("cellular", 9);
+    if (phone_error.length && cellular_error.length) {
+        errors.push(
+            "Укажите правильно хотя бы один из телефонов! Домашний " +
+            phone_error + "Мобильный " + cellular_error
+        );
+    }
 }
 
 function xsm_ank_check_empty(id, sDesc, aResult) {
@@ -122,22 +139,22 @@ function xsm_ank_setup_fizlesh() {
     $("#hobby-text").change(xsm_ank_on_change);
 
     $("#submit_anketa-submit").click(function() {
-        var aError = [];
-        xsm_ank_check_name(aError, "last_name", "Фамилия");
-        xsm_ank_check_name(aError, "first_name", "Имя");
-        xsm_ank_check_name(aError, "patronymic", "Отчество");
-        xsm_ank_check_birth_date(aError, "birth_date", "Дата рождения");
-        xsm_ank_check_class(aError);
-        xsm_ank_check_phones(aError);
-        xsm_ank_check_email(aError);
-        xsm_ank_check_control_question(aError, "fizlesh");
+        var errors = [];
+        xsm_ank_check_name(errors, "last_name", "Фамилия");
+        xsm_ank_check_name(errors, "first_name", "Имя");
+        xsm_ank_check_name(errors, "patronymic", "Отчество");
+        xsm_ank_check_birth_date(errors, "birth_date", "Дата рождения");
+        xsm_ank_check_class(errors);
+        xsm_ank_check_phones(errors);
+        xsm_ank_check_email(errors);
+        xsm_ank_check_control_question(errors, "fizlesh");
 
         var aWarning = [];
         WarnPersonal(aWarning);
 
         var bResult = true;
-        if (aError.length) {
-            $("#t-Error").html(xsm_ank_make_list(aError));
+        if (errors.length) {
+            $("#t-Error").html(xsm_ank_make_list(errors));
             bResult = false;
         } else {
             $("#t-Error").html(' ');
@@ -160,17 +177,17 @@ function xsm_ank_setup_fizlesh() {
         } else {
             $("#t-Warning").html(' ');
         }
-        if (!aError.length && !aWarning.length) $("#c-Message").hide();
+        if (!errors.length && !aWarning.length) $("#c-Message").hide();
         else $("#c-Message").show();
 
         return bResult;
     });
 }
 
-function xsm_ank_collect_phone_errors(errors, element_id) {
+function xsm_ank_collect_phone_errors(errors, element_id, title) {
     var phone_error = xsm_ank_check_phone(element_id, 9);
     if (phone_error.length) {
-        errors.push(phone_error);
+        errors.push(title + " " + phone_error);
     }
 }
 
@@ -182,9 +199,8 @@ function xsm_ank_setup_med_olymp() {
         xsm_ank_check_name(errors, "patronymic", "Отчество");
         xsm_ank_check_birth_date(errors, "birth_date", "Дата рождения");
         xsm_ank_check_class(errors);
-        xsm_ank_collect_phone_errors(errors, "cellular");
-        xsm_ank_collect_phone_errors(errors, "parent_phone");
-        xsm_ank_check_phones(errors);
+        xsm_ank_collect_phone_errors(errors, "cellular", "Мобильный");
+        xsm_ank_collect_phone_errors(errors, "parent_phone", "Родительский");
         xsm_ank_check_name(errors, "parent_fio", "ФИО родителя");
         xsm_ank_check_email(errors, "email");
         xsm_ank_check_email(errors, "parent_email");
@@ -209,12 +225,10 @@ function xsm_ank_setup_med_olymp() {
 
 
 $(document).ready(function() {
-    if ($('#favourites-text')) {
+    if ($('#favourites-text').length) {
         // FizLesh anketa, default
         xsm_ank_setup_fizlesh();
-        console.log("qqq");
-    } else if ($('#department_id-input').val() == "8") {
+    } else if ($('#department_id-hidden').val() == "8") {
         xsm_ank_setup_med_olymp();
-        console.log("zzz");
     }
 });
