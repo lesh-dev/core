8dbe0f638681bd7fc11a1c4cc94060d2b61dd0fa
Yar <yarebenko@gmail.com>
2018-01-21 00:15:20 +0300

    orm

--- yasm/core.py
+++ yasm/core.py
@@ -1,22 +1,29 @@
-import yasm.db as db
+from yasm.db import *
 from flask import Flask
+from yasm.test import Test
+import yasm.config as cfg
 
 if __name__ == "__main__":
     app = Flask(__name__)
-    db.connect(app)
+    app.config['SQLALCHEMY_DATABASE_URI'] = cfg.db_uri
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    database.init_app(app)
+    app.app_context().push()
     # testing if all objects are readable
-    for i in (db.Notification,
-              db.Department,
-              db.Person,
-              db.Course,
-              db.CourseTeachers,
-              db.Exam,
-              db.School,
-              db.PersonSchool,
-              db.PersonComment,
-              db.Submission,
-              db.Contestants,
-              db.Problems,
-              db.Solutions):
+    for i in (Notification,
+              Department,
+              Person,
+              Course,
+              CourseTeachers,
+              Exam,
+              School,
+              PersonSchool,
+              PersonComment,
+              Submission,
+              Contestants,
+              Problems,
+              Solutions):
         print(i.query.all())
+
+    app.add_url_rule('/', view_func=Test.as_view('Greet'))
     app.run()
