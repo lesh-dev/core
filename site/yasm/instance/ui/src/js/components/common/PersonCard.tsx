95e68856a90d426ea6dde0f8879ca5aaa26de197
rebenkoy <r.redmage@yandex.ru>
2018-07-19 21:47:42 +0300

    person cards in school dash

--- site/yasm/instance/ui/src/js/components/common/PersonCard.tsx
+++ site/yasm/instance/ui/src/js/components/common/PersonCard.tsx
@@ -14,6 +14,7 @@ export interface PersonCardProps {
 
 export class PersonCard extends React.Component<PersonCardProps> {
     render() {
+        console.log(this.props.person);
         return <div className={"person_card" + (this.props.clickable ? " person_card--clickable" : "")}
                     style={this.props.style} onClick={() => {
             if (this.props.clickable) {
