21b4214d15993c086918a9aef1b456384dfdc012
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2018-09-12 08:40:54 +0300

    Class increaser seems to be working

---
+++ site/increase_class.php
@@ -0,0 +1,15 @@
+<?php
+
+$engine_dir = "engine/";
+$xengine_dir = "xengine/";
+
+$SETTINGS["content_dir"] = "content/";
+$SETTINGS['xsm_db_name'] = 'content/ank/fizlesh.sqlite3';
+
+session_start();
+
+require_once("${xengine_dir}sys/db.php");
+require_once("${engine_dir}cms/ank/format.php");
+require_once("${engine_dir}cms/ank/person.php");
+
+xsm_increase_class_numbers();
