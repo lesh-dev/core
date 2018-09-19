1bd4580c0d7382c97c67f58f48459aeef675f019
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2018-09-19 12:21:16 +0300

    Better tests

--- tests/xcms_xsm_bulk_actions.py
+++ tests/xcms_xsm_bulk_actions.py
@@ -16,6 +16,53 @@ class XcmsXsmBulkActions(xsm.Manager, xtest_common.XcmsTest):
 
     def test_bulk_archive(self):
         # positive part: check that button is accessible by admin and works
+        self.test_bulk_archive_impl()
+        self.test_bulk_archive_impl(admin_mode=False)
+
+    def test_bulk_archive_impl(self, admin_mode=True):
+        self.ensure_logged_off()
+        self.goto_root()
+        if admin_mode:
+            self.perform_login_as_admin()
+        else:
+            self.perform_login_as_manager()
+        self.goto_root()
+        self.goto_xsm()
+        self.goto_xsm_all_people()
+        self.goto_xsm_add_person()
+
+        person = xsm.Person(self)
+        person.input(
+            last_name=u"Архивариус",
+            first_name=u"Пётр",
+            patronymic=u"Семёнович",
+            anketa_status="declined",
+            random=True,
+        )
+        person.back_to_person_view()
+        self.assertElementTextById("anketa_status-span", u"Отклонён")
+        self.goto_xsm_anketas()
+
+        self.clickElementById("bulk_archive-submit")
+        self.wait(5, "Wait while form resubmits")
+        self.check_page_errors()
+
+        self.goto_xsm_anketas()
+        if admin_mode:
+            self.assertTextNotPresent(
+                xpath="//table[class='ankList']",
+                text=u"Отклонён",
+            )
+        else:
+            self.assertTextPresent(
+                xpath="//table[class='ankList']",
+                text=u"Отклонён",
+            )
+
+    def test_increase_class(self):
+        # TODO: implement
+        return
+        # positive part: check that button is accessible by admin and works
         self.ensure_logged_off()
         self.goto_root()
         self.goto_anketa()
@@ -43,4 +90,7 @@ class XcmsXsmBulkActions(xsm.Manager, xtest_common.XcmsTest):
         self.check_page_errors()
 
         self.goto_xsm_anketas()
-        self.assertBodyTextNotPresent(u"Отклонён")
+        self.assertTextNotPresent(
+            xpath="//table[class='ankList']",
+            text=u"Отклонён",
+        )
