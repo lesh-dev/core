8df37e0489230b14a971750d9986d0392ca05e9c
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2018-10-17 11:17:30 +0300

    Properly use settings

--- site/increase_class.php
+++ site/increase_class.php
@@ -5,14 +5,15 @@
 $engine_dir = "engine/";
 $xengine_dir = "xengine/";
 
-$SETTINGS["content_dir"] = "content/";
-$SETTINGS['xsm_db_name'] = 'content/ank/fizlesh.sqlite3';
-
 session_start();
 
 require_once("${xengine_dir}sys/db.php");
 require_once("${engine_dir}cms/ank/format.php");
 require_once("${engine_dir}cms/ank/person.php");
+
+$SETTINGS["content_dir"] = "content/";
+$SETTINGS[XDB_DB_NAME] = 'content/ank/fizlesh.sqlite3';
+$SETTINGS[XDB_DB_TYPE] = XDB_DB_TYPE_SQLITE3;
 
 xsm_increase_class_numbers();
 
