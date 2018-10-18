f3a3ecbca1ad31f4e99f9f52ad8d0aa04a0263b8
Yaroslav <yarebenko@gmail.com>
2018-10-18 12:52:39 +0300

    speed tests login fix

--- site/yasm/tests/TestSpeed/generated.py
+++ site/yasm/tests/TestSpeed/generated.py
@@ -3,6 +3,8 @@ import timeit
 import sys
 import json
 import instance
+import instance.login.controllers
+from flask_login import login_user
 from instance.api.generated import notification_list
 from instance.api.generated import department_list
 from instance.api.generated import person_list
@@ -23,13 +25,16 @@ class TestSpeed(unittest.TestCase):
     @classmethod
     def test_notification_list(cls):
         with yasm.test_request_context():
-            time = timeit.timeit(notification_list, number=100)
+            login_user(user=instance.login.controllers.load_user(467))
+            time = timeit.timeit(notification_list, number=10)
             test_case = sys._getframe().f_code.co_name
             results_file = open("results")
             results = json.load(results_file)
             results_file.close()
             if test_case in results.keys():
                 assert results[test_case] * 1.1 > time
+                if time < results[test_case]:
+                    results[test_case] = time
             else:
                 results[test_case] = time
             results_file = open("results", "w")
@@ -39,13 +44,16 @@ class TestSpeed(unittest.TestCase):
     @classmethod
     def test_department_list(cls):
         with yasm.test_request_context():
-            time = timeit.timeit(department_list, number=100)
+            login_user(user=instance.login.controllers.load_user(467))
+            time = timeit.timeit(department_list, number=10)
             test_case = sys._getframe().f_code.co_name
             results_file = open("results")
             results = json.load(results_file)
             results_file.close()
             if test_case in results.keys():
                 assert results[test_case] * 1.1 > time
+                if time < results[test_case]:
+                    results[test_case] = time
             else:
                 results[test_case] = time
             results_file = open("results", "w")
@@ -55,13 +63,16 @@ class TestSpeed(unittest.TestCase):
     @classmethod
     def test_person_list(cls):
         with yasm.test_request_context():
-            time = timeit.timeit(person_list, number=100)
+            login_user(user=instance.login.controllers.load_user(467))
+            time = timeit.timeit(person_list, number=10)
             test_case = sys._getframe().f_code.co_name
             results_file = open("results")
             results = json.load(results_file)
             results_file.close()
             if test_case in results.keys():
                 assert results[test_case] * 1.1 > time
+                if time < results[test_case]:
+                    results[test_case] = time
             else:
                 results[test_case] = time
             results_file = open("results", "w")
@@ -71,13 +82,16 @@ class TestSpeed(unittest.TestCase):
     @classmethod
     def test_contact_list(cls):
         with yasm.test_request_context():
-            time = timeit.timeit(contact_list, number=100)
+            login_user(user=instance.login.controllers.load_user(467))
+            time = timeit.timeit(contact_list, number=10)
             test_case = sys._getframe().f_code.co_name
             results_file = open("results")
             results = json.load(results_file)
             results_file.close()
             if test_case in results.keys():
                 assert results[test_case] * 1.1 > time
+                if time < results[test_case]:
+                    results[test_case] = time
             else:
                 results[test_case] = time
             results_file = open("results", "w")
@@ -87,13 +101,16 @@ class TestSpeed(unittest.TestCase):
     @classmethod
     def test_school_list(cls):
         with yasm.test_request_context():
-            time = timeit.timeit(school_list, number=100)
+            login_user(user=instance.login.controllers.load_user(467))
+            time = timeit.timeit(school_list, number=10)
             test_case = sys._getframe().f_code.co_name
             results_file = open("results")
             results = json.load(results_file)
             results_file.close()
             if test_case in results.keys():
                 assert results[test_case] * 1.1 > time
+                if time < results[test_case]:
+                    results[test_case] = time
             else:
                 results[test_case] = time
             results_file = open("results", "w")
@@ -103,13 +120,16 @@ class TestSpeed(unittest.TestCase):
     @classmethod
     def test_course_list(cls):
         with yasm.test_request_context():
-            time = timeit.timeit(course_list, number=100)
+            login_user(user=instance.login.controllers.load_user(467))
+            time = timeit.timeit(course_list, number=10)
             test_case = sys._getframe().f_code.co_name
             results_file = open("results")
             results = json.load(results_file)
             results_file.close()
             if test_case in results.keys():
                 assert results[test_case] * 1.1 > time
+                if time < results[test_case]:
+                    results[test_case] = time
             else:
                 results[test_case] = time
             results_file = open("results", "w")
@@ -119,13 +139,16 @@ class TestSpeed(unittest.TestCase):
     @classmethod
     def test_course_teachers_list(cls):
         with yasm.test_request_context():
-            time = timeit.timeit(course_teachers_list, number=100)
+            login_user(user=instance.login.controllers.load_user(467))
+            time = timeit.timeit(course_teachers_list, number=10)
             test_case = sys._getframe().f_code.co_name
             results_file = open("results")
             results = json.load(results_file)
             results_file.close()
             if test_case in results.keys():
                 assert results[test_case] * 1.1 > time
+                if time < results[test_case]:
+                    results[test_case] = time
             else:
                 results[test_case] = time
             results_file = open("results", "w")
@@ -135,13 +158,16 @@ class TestSpeed(unittest.TestCase):
     @classmethod
     def test_exam_list(cls):
         with yasm.test_request_context():
-            time = timeit.timeit(exam_list, number=100)
+            login_user(user=instance.login.controllers.load_user(467))
+            time = timeit.timeit(exam_list, number=10)
             test_case = sys._getframe().f_code.co_name
             results_file = open("results")
             results = json.load(results_file)
             results_file.close()
             if test_case in results.keys():
                 assert results[test_case] * 1.1 > time
+                if time < results[test_case]:
+                    results[test_case] = time
             else:
                 results[test_case] = time
             results_file = open("results", "w")
@@ -151,13 +177,16 @@ class TestSpeed(unittest.TestCase):
     @classmethod
     def test_person_school_list(cls):
         with yasm.test_request_context():
-            time = timeit.timeit(person_school_list, number=100)
+            login_user(user=instance.login.controllers.load_user(467))
+            time = timeit.timeit(person_school_list, number=10)
             test_case = sys._getframe().f_code.co_name
             results_file = open("results")
             results = json.load(results_file)
             results_file.close()
             if test_case in results.keys():
                 assert results[test_case] * 1.1 > time
+                if time < results[test_case]:
+                    results[test_case] = time
             else:
                 results[test_case] = time
             results_file = open("results", "w")
@@ -167,13 +196,16 @@ class TestSpeed(unittest.TestCase):
     @classmethod
     def test_person_comment_list(cls):
         with yasm.test_request_context():
-            time = timeit.timeit(person_comment_list, number=100)
+            login_user(user=instance.login.controllers.load_user(467))
+            time = timeit.timeit(person_comment_list, number=10)
             test_case = sys._getframe().f_code.co_name
             results_file = open("results")
             results = json.load(results_file)
             results_file.close()
             if test_case in results.keys():
                 assert results[test_case] * 1.1 > time
+                if time < results[test_case]:
+                    results[test_case] = time
             else:
                 results[test_case] = time
             results_file = open("results", "w")
@@ -183,13 +215,16 @@ class TestSpeed(unittest.TestCase):
     @classmethod
     def test_submission_list(cls):
         with yasm.test_request_context():
-            time = timeit.timeit(submission_list, number=100)
+            login_user(user=instance.login.controllers.load_user(467))
+            time = timeit.timeit(submission_list, number=10)
             test_case = sys._getframe().f_code.co_name
             results_file = open("results")
             results = json.load(results_file)
             results_file.close()
             if test_case in results.keys():
                 assert results[test_case] * 1.1 > time
+                if time < results[test_case]:
+                    results[test_case] = time
             else:
                 results[test_case] = time
             results_file = open("results", "w")
@@ -199,13 +234,16 @@ class TestSpeed(unittest.TestCase):
     @classmethod
     def test_contestants_list(cls):
         with yasm.test_request_context():
-            time = timeit.timeit(contestants_list, number=100)
+            login_user(user=instance.login.controllers.load_user(467))
+            time = timeit.timeit(contestants_list, number=10)
             test_case = sys._getframe().f_code.co_name
             results_file = open("results")
             results = json.load(results_file)
             results_file.close()
             if test_case in results.keys():
                 assert results[test_case] * 1.1 > time
+                if time < results[test_case]:
+                    results[test_case] = time
             else:
                 results[test_case] = time
             results_file = open("results", "w")
@@ -215,13 +253,16 @@ class TestSpeed(unittest.TestCase):
     @classmethod
     def test_problems_list(cls):
         with yasm.test_request_context():
-            time = timeit.timeit(problems_list, number=100)
+            login_user(user=instance.login.controllers.load_user(467))
+            time = timeit.timeit(problems_list, number=10)
             test_case = sys._getframe().f_code.co_name
             results_file = open("results")
             results = json.load(results_file)
             results_file.close()
             if test_case in results.keys():
                 assert results[test_case] * 1.1 > time
+                if time < results[test_case]:
+                    results[test_case] = time
             else:
                 results[test_case] = time
             results_file = open("results", "w")
@@ -231,13 +272,16 @@ class TestSpeed(unittest.TestCase):
     @classmethod
     def test_solutions_list(cls):
         with yasm.test_request_context():
-            time = timeit.timeit(solutions_list, number=100)
+            login_user(user=instance.login.controllers.load_user(467))
+            time = timeit.timeit(solutions_list, number=10)
             test_case = sys._getframe().f_code.co_name
             results_file = open("results")
             results = json.load(results_file)
             results_file.close()
             if test_case in results.keys():
                 assert results[test_case] * 1.1 > time
+                if time < results[test_case]:
+                    results[test_case] = time
             else:
                 results[test_case] = time
             results_file = open("results", "w")
