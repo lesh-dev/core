70575ed2adebc4e8092305e091bd201eb908de64
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2018-10-24 09:57:43 +0300

    Fix session init in unittest wrapper

--- site/unittest.php
+++ site/unittest.php
@@ -1,19 +1,20 @@
 <?php
-    date_default_timezone_set('Europe/Moscow');
-    require_once("settings.php");
-    require_once("${xengine_dir}sys/settings.php");
-    require_once("${xengine_dir}sys/file.php");
-    require_once("${xengine_dir}sys/unittest.php");
-    require_once("${xengine_dir}sys/util.php");
-    require_once("${xengine_dir}sys/db.php");
-    require_once("${xengine_dir}sys/controls.php");
-    require_once("${xengine_dir}sys/auth.php");
-    require_once("${xengine_dir}sys/diff/diff-utils.php");
-    require_once("${engine_dir}cms/ank/format.php");
-    require_once("${engine_dir}cms/ank/ank-proc.php");
-    require_once("${engine_dir}cms/ank/field-desc.php");
+session_start();
+date_default_timezone_set('Europe/Moscow');
+require_once("settings.php");
+require_once("${xengine_dir}sys/settings.php");
+require_once("${xengine_dir}sys/file.php");
+require_once("${xengine_dir}sys/unittest.php");
+require_once("${xengine_dir}sys/util.php");
+require_once("${xengine_dir}sys/db.php");
+require_once("${xengine_dir}sys/controls.php");
+require_once("${xengine_dir}sys/auth.php");
+require_once("${xengine_dir}sys/diff/diff-utils.php");
+require_once("${engine_dir}cms/ank/format.php");
+require_once("${engine_dir}cms/ank/ank-proc.php");
+require_once("${engine_dir}cms/ank/field-desc.php");
 
-    header("Content-Type: text/html; charset=utf-8");
+header("Content-Type: text/html; charset=utf-8");
 ?><!DOCTYPE html>
 <html>
 <head>
@@ -21,19 +22,19 @@
     <title>XCMS Unit Tests</title>
 </head>
 <body><?php
-    xut_initialize();
+xut_initialize();
 
-    // put your unit test function call here
-    xdb_unit_test();
-    xcms_util_unit_test();
-    xcms_string_unit_test();
-    xcms_sys_controls_unit_test();
-    xcms_keyvalue_unit_test();
-    xcms_finediff_unit_test();
-    xsm_ank_format_unit_test();
-    xsm_ank_proc_unit_test();
-    xsm_field_desc_unit_test();
-    XcmsUser::unit_test();
+// put your unit test function call here
+xdb_unit_test();
+xcms_util_unit_test();
+xcms_string_unit_test();
+xcms_sys_controls_unit_test();
+xcms_keyvalue_unit_test();
+xcms_finediff_unit_test();
+xsm_ank_format_unit_test();
+xsm_ank_proc_unit_test();
+xsm_field_desc_unit_test();
+XcmsUser::unit_test();
 
-    xut_finalize();
+xut_finalize();
 ?></body></html>
