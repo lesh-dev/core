c4841d38a2eb689699dfb498f52a2c0baa31b831
Alexey Golovko <i@math-lesh.org>
2018-10-26 17:17:23 +0300

    Сделал миграцию данных одной транзакцией вместо трёх, исключил таблицу users.

--- site/yasm/sqlite_to_postgresql/dump-data.sh
+++ site/yasm/sqlite_to_postgresql/dump-data.sh
@@ -6,7 +6,7 @@ filename="$1"
 tables=$(echo .schema | sqlite3 "$filename" | grep -Eo 'CREATE TABLE( IF NOT EXISTS)? [^\(]+' | sed -e 's/CREATE TABLE//' -e 's/IF NOT EXISTS//')
 
 # Но нам нужен определённый порядок
-for tbl in school contestants problems solutions notification department course person course_teachers person_school exam submission xversion person_comment pss contact users; do
+for tbl in school contestants problems solutions notification department course person course_teachers person_school exam submission xversion person_comment pss contact; do
     #echo "dumping $tbl" >&2
-    (echo .mode insert; echo "SELECT * FROM $tbl;") | sqlite3 "$filename" | sed -e "s/INSERT INTO \"table\"/INSERT INTO $tbl/" -e "s/INSERT INTO table/INSERT INTO $tbl/"
+    (echo .mode insert; echo "SELECT * FROM $tbl;") | sqlite3 "$filename" | sed -e "s/INSERT INTO \"table\"/INSERT INTO public.$tbl/" -e "s/INSERT INTO table/INSERT INTO public.$tbl/"
 done;
