87c0d8e0786eda49a1fc6e34238e4046816dd3df
Yaroslav <yarebenko@gmail.com>
2018-10-26 12:36:47 +0300

    testinglib load result fixed

--- site/yasm/tests/testinglib/load_result.py
+++ site/yasm/tests/testinglib/load_result.py
@@ -15,11 +15,10 @@ class Loader(object):
             result.set(self.results[self.test_case] if self.test_case in self.results.keys() else None)
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-
         with open("tests/results", "w") as results_file:
             if (not self.test_case in self.results.keys() and result.get() != None) or result.get() != self.results[self.test_case]:
                 self.results[self.test_case] = result.get()
-                results_file.write(json.dumps(self.results, indent=4, sort_keys=True))
+            results_file.write(json.dumps(self.results, indent=4, sort_keys=True))
             if exc_val:
                 raise exc_val
 
