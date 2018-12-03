d73e40082a07577adb252ad694d48528aa61d65a
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2018-12-03 17:29:05 +0300

    Switch to full*engine path

--- site/index.php
+++ site/index.php
@@ -2,10 +2,6 @@
 
 session_set_cookie_params(365 * 24 * 3600); // 2 weeks
 $session_result = @session_start();
-if (file_exists("install.php") && filesize("install.php") > 0) {
-    header("Location: install.php");
-    exit(0);
-}
 header("Content-Type: text/html; charset=utf-8");
 date_default_timezone_set('Europe/Moscow');
 
@@ -16,34 +12,32 @@ if (!file_exists("settings.php")) {?>
         </head>
         <body>
             <h1>XCMS Configuration Error</h1>
-            <p>Cannot find configuration (<tt>settings.php</tt>). You must either
-            run installer to generate it or write it manually.
-            To run installer just rename <tt>install.nt.php</tt>
-            to <tt>install.php</tt> and refresh current page.</p>
+            <p>Cannot find configuration (<tt>settings.php</tt>). You should
+            create it by hands using sample configuration.</p>
         </body>
     </html><?php
     die();
 }
-require_once ("settings.php");
+require_once("settings.php");
 
-require_once ("${xengine_dir}sys/settings.php");
-require_once ("${xengine_dir}sys/string.php");
-require_once ("${xengine_dir}sys/tag.php");
-require_once ("${xengine_dir}sys/cms.php");
-require_once ("${xengine_dir}sys/file.php");
-require_once ("${xengine_dir}sys/util.php");
-require_once ("${xengine_dir}sys/unittest.php");
-require_once ("${xengine_dir}sys/auth.php");
-require_once ("${xengine_dir}sys/logger.php");
-require_once ("${xengine_dir}sys/attachment.php");
-require_once ("${xengine_dir}sys/compiler.php");
-require_once ("${xengine_dir}sys/registry.php");
-require_once ("${xengine_dir}sys/mailer.php");
-require_once ("${xengine_dir}sys/resample.php");
-require_once ("${xengine_dir}sys/controls.php");
+require_once("${full_xengine_dir}sys/settings.php");
+require_once("${full_xengine_dir}sys/string.php");
+require_once("${full_xengine_dir}sys/tag.php");
+require_once("${full_xengine_dir}sys/cms.php");
+require_once("${full_xengine_dir}sys/file.php");
+require_once("${full_xengine_dir}sys/util.php");
+require_once("${full_xengine_dir}sys/unittest.php");
+require_once("${full_xengine_dir}sys/auth.php");
+require_once("${full_xengine_dir}sys/logger.php");
+require_once("${full_xengine_dir}sys/attachment.php");
+require_once("${full_xengine_dir}sys/compiler.php");
+require_once("${full_xengine_dir}sys/registry.php");
+require_once("${full_xengine_dir}sys/mailer.php");
+require_once("${full_xengine_dir}sys/resample.php");
+require_once("${full_xengine_dir}sys/controls.php");
 
 # old engine parts
-require_once ("${engine_dir}cms/alias.php");
+require_once("${full_engine_dir}cms/alias.php");
 
 if (!$session_result) {
     xcms_log(XLOG_ERROR, "[SESSION] Session start failed");
--- site/settings.production-fizlesh.ru.php
+++ site/settings.production-fizlesh.ru.php
@@ -1,35 +1,62 @@
 <?php
-    global $SETTINGS;
+global $SETTINGS;
 
-    // This, in particular, disables some unauthorized handlers
-    $SETTINGS["production"] = true;
+// This, in particular, disables some unauthorized handlers
+$SETTINGS["production"] = true;
 
-    global $content_dir;
-    $content_dir = "content/";
-    global $design_dir;
-    $design_dir = "fizlesh.ru-design/";
-    global $engine_dir;
-    $engine_dir = "engine/";
-    global $xengine_dir;
-    $xengine_dir = "xengine/";
-    global $engine_pub;
-    $engine_pub = "engine_public/";
-    global $web_prefix;
-    $web_prefix = "";
-    $mailer_enabled = true;
-    $SETTINGS['content_time_roundup'] = 100;
+global $content_dir;
+$content_dir = "content/";
 
-    $SETTINGS['auth_vk_id'] = '';
-    $SETTINGS['auth_vk_rights'] = 'notify,offline';
+global $design_dir;
+$design_dir = "fizlesh.ru-design/";
 
-    global $meta_site_name;
-    $meta_site_name = 'ФизЛЭШ';
-    global $meta_site_url;
-    $meta_site_url = 'http://fizlesh.ru/';
-    global $meta_site_log_path;
-    $meta_site_log_path = '/var/log/xcms/fizlesh.ru-engine.log';
-    global $meta_site_mail;
-    $meta_site_mail = 'support@fizlesh.ru';
+global $engine_dir;
+$engine_dir = "engine/";
 
-    $SETTINGS['xsm_db_name'] = 'ank/fizlesh.sqlite3';
-?>
+global $xengine_dir;
+$xengine_dir = "xengine/";
+
+global $engine_pub;
+$engine_pub = "engine_public/";
+
+global $web_prefix;
+$web_prefix = "";
+
+$mailer_enabled = true;
+
+// DO NOT EDIT THIS PART
+// UNLESS YOU UNDERSTAND
+// WHAT ARE YOU DOING!
+global $full_web_prefix;
+$full_web_prefix = "${_SERVER["DOCUMENT_ROOT"]}/${web_prefix}";
+
+global $full_xengine_dir;
+$full_xengine_dir = "${full_web_prefix}${xengine_dir}";
+
+global $full_engine_dir;
+$full_xengine_dir = "${full_web_prefix}${xengine_dir}";
+
+require_once("${full_xengine_dir}sys/db_const.php");
+// END OF IMMUTABLE PART
+
+$SETTINGS['content_time_roundup'] = 100;
+$SETTINGS['auth_vk_id'] = '';
+$SETTINGS['auth_vk_rights'] = 'notify,offline';
+
+global $meta_site_name;
+$meta_site_name = 'ФизЛЭШ';
+
+global $meta_site_url;
+$meta_site_url = 'http://fizlesh.ru/';
+
+global $meta_site_log_path;
+$meta_site_log_path = '/var/log/xcms/fizlesh.ru-engine.log';
+
+global $meta_site_mail;
+$meta_site_mail = 'support@fizlesh.ru';
+
+$SETTINGS['xsm_db_name'] = 'ank/fizlesh.sqlite3';
+
+// nextgen: postgres
+// $SETTINGS[XDB_DB_TYPE] = XDB_DB_TYPE_PG;
+// $SETTINGS[XDB_DB_NAME] = "host=localhost user=lesh password=123 dbname=lesh";
