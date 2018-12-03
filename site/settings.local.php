31ae0baa02bed41814db585d4d39d96049a30d19
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2018-12-03 17:27:21 +0300

    Update local settings after full*engine hacks

--- site/settings.local.php
+++ site/settings.local.php
@@ -1,21 +1,42 @@
 <?php
 
 global $SETTINGS;
+
 global $content_dir;
 $content_dir = "content/";
+
 global $design_dir;
 $design_dir = "fizlesh.ru-design/";
+
 global $engine_dir;
 $engine_dir = "engine/";
+
 global $xengine_dir;
 $xengine_dir = "xengine/";
+
 global $engine_pub;
 $engine_pub = "engine_public/";
+
 global $web_prefix;
 $web_prefix = "";
+
+global $mailer_enabled;
 $mailer_enabled = false;
 
-require_once("${xengine_dir}sys/db.php");
+// DO NOT EDIT THIS PART
+// UNLESS YOU UNDERSTAND
+// WHAT ARE YOU DOING!
+global $full_web_prefix;
+$full_web_prefix = "${_SERVER["DOCUMENT_ROOT"]}/${web_prefix}";
+
+global $full_xengine_dir;
+$full_xengine_dir = "${full_web_prefix}${xengine_dir}";
+
+global $full_engine_dir;
+$full_xengine_dir = "${full_web_prefix}${xengine_dir}";
+
+require_once("${full_xengine_dir}sys/db_const.php");
+// END OF IMMUTABLE PART
 
 $SETTINGS['content_time_roundup'] = 100;
 $SETTINGS['auth_vk_id'] = '';
@@ -23,10 +44,13 @@ $SETTINGS['auth_vk_rights'] = 'notify,offline';
 
 global $meta_site_name;
 $meta_site_name = 'fizlesh.local';
+
 global $meta_site_url;
 $meta_site_url = 'http://fizlesh.local/';
+
 global $meta_site_log_path;
 $meta_site_log_path = '/var/log/xcms/fizlesh.local-engine.log';
+
 global $meta_site_mail;
 $meta_site_mail = 'support@fizlesh.local';
 
