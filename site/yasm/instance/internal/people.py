bf3b287cf88bb2ed04c33e14904f402dc6d7c026
rebenkoy <yarebenko@gmail.com>
2020-04-18 23:44:42 +0000

    ava loading for other

--- site/yasm/instance/internal/people.py
+++ site/yasm/instance/internal/people.py
@@ -1,5 +1,6 @@
 from instance.generated.api.yasm.internal.person.api_people import Interface
 from instance.generated.models.yasm.database import Person
+from instance.generated.enums.yasm.database import DatabaseStatus
 
 
 class APIPeople(Interface):
@@ -7,4 +8,6 @@ class APIPeople(Interface):
     def fetch_person(
         request,
     ):
-        return Person.query.get(int(request.id))
+        person = Person.query.get(int(request.id))
+        person.avas = [ava for ava in person.avas if ava.status == DatabaseStatus.relevant]
+        return person
